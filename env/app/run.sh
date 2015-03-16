9cf1b711334abba9364b3543ae4acff381a67a6b
Richard Olsson <r@richardolsson.se>
2015-03-16 15:08:35 +0100

    Edit Runit run script so service can be restarted.

--- env/app/run.sh
+++ env/app/run.sh
@@ -5,4 +5,5 @@ cd /var/app
 # Install requirements on first run. This is not suitable for deployment, for
 # which we shuld instead COPY the repository into the image at build-time.
 npm install
-exec npm start
+exec 2>&1
+exec node app.js
