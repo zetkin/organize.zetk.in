206f3e4b31031ec58f6d667cdfaaf8d2e7227b0d
Richard Olsson <r@richardolsson.se>
2015-03-16 15:12:36 +0100

    Add /api route which redirects to back-end.
    All URIs below this route will just forward the HTTP I/O to the API backend
    defined by the environment variables API_HOST and API_PORT. This is just a
    skeleton which will need to be developed further to implement it's own security
    and validation.

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 express = require('express');
+http = require('http');
 
 app = express();
+
+api = express.Router()
+api.all(/(.*)/, function(req, res) {
+    // TODO: Verify data?
+    var options = {
+        // TODO: Handle any verb (check req.method)
+        host: process.env.API_HOST,
+        port: process.env.API_PORT,
+        path: req.path
+    };
+
+    var request = http.request(options, function(response) {
+        // TODO: Reserialize data?
+        response.setEncoding('utf8');
+        response.on('data', function(chunk) {
+            res.send(chunk);
+        });
+    });
+
+    request.end();
+});
+
+app.use('/api', api);
 
 app.get('/', function(req, res) {
     res.set('Content-Type', 'text/plain');
