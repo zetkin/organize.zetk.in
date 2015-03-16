cbbe5c844723bb8b9c87667da634080ae36a65ee
Richard Olsson <r@richardolsson.se>
2015-03-16 15:09:56 +0100

    Configure API back-end when starting container.
    If the api container is running, bind to that and set environment variables to
    connect to that container directly. If it's not running, use (currently
    non-existent) dev server as an API back-end. To be used by developers who do
    not have access to the Zetkin platform code.

--- env/up.sh
+++ env/up.sh
@@ -19,25 +19,40 @@ run_container() {
     docker run --name=$1 $2 $1
 }
 
-container_up() {
-    # Param $1: name of image/container
-    # Param $2: command line args
+container_is_running() {
+    # Param $1: name of container
     running=`docker inspect -f "{{.State.Running}}" $1`
     if [ $? = 0 ]; then
         if [ "$running" = "true" ]; then
-            echo "skipping $1 (already running)"
-        else
-            if [ "$running" = "false" ]; then
-                echo removing old $1
-                docker rm -f $1 2>&1 > /dev/null
-            fi
-
-            run_container $1 "$2"
+            return 1
         fi
+    fi
+
+    return 0
+}
+
+container_up() {
+    # Param $1: name of image/container
+    # Param $2: command line args
+    container_is_running $1
+    if [ $? = 1 ]; then
+        echo "skipping $1 (already running)"
     else
+        docker rm -f $1 2>&1 > /dev/null
         run_container $1 "$2"
     fi
 }
 
 build_if_not_exists zetkin-od app
-container_up zetkin-od "-ti --rm -p 80:80 -v $PWD/..:/var/app"
+
+# Check whether there is a locally running container for the Zetkin platform.
+# If not, use a development server as API back-end
+container_is_running api
+if [ $? = 1 ]; then
+    api_params='-e API_HOST=api -e API_PORT=8080 --link api:api'
+else
+    api_params='-e API_HOST=dev.zetk.in -e API_PORT=80'
+fi
+
+echo Starting with API settings: $api_params
+container_up zetkin-od "-ti --rm -p 80:80 -v $PWD/..:/var/app $api_params"
