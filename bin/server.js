e5e882bc219e95349c2cdc45bc29bd2006f9a809
Richard Olsson <r@richardolsson.se>
2015-07-21 11:49:42 +0200

    Remove old entry points.

--- bin/server.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var app = require('../zetkin');
-
-var server = app.listen(8000, function() {
-    var addr = server.address();
-    console.log('Listening on http://%s:%s', addr.address, addr.port);
-});
