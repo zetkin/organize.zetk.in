ef778d0fa699f2510ab77814b3445e85138f4ae1
Richard Olsson <r@richardolsson.se>
2015-07-21 12:12:31 +0200

    Remove old unused files
    The test suite will need to be updated.

--- gulp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Runs locally installed gulp, so that gulp doesn't need to be installed on the
-# host system. The local gulp and other packages in node_modules should be
-# installed by the ./env/setup.sh script, which installs them from within the
-# docker container.
-./node_modules/.bin/gulp $@
