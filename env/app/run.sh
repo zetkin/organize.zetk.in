ae0e44b0c5e222dd299bf1893afc196a17157c87
Richard Olsson <r@richardolsson.se>
2015-07-21 12:12:13 +0200

    Add new run script to Docker container

--- env/app/run.sh
+++ env/app/run.sh
@@ -2,7 +2,5 @@
 
 cd /var/app
 
-# Install requirements on first run. This is not suitable for deployment, for
-# which we shuld instead COPY the repository into the image at build-time.
 exec 2>&1
-exec node bin/server.js
+exec node dist/organize.zetk.in/server/main.js
