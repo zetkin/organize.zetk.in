1463abbb836569b58343d22299b9902ca97b21b5
Richard Olsson <r@richardolsson.se>
2015-07-21 16:11:12 +0200

    Implement widgets in dashboard

--- src/js/components/dashboard/Dashboard.jsx
+++ src/js/components/dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react/addons';
 
 import FluxComponent from '../FluxComponent';
 import Shortcut from './Shortcut';
+import UpcomingCampaignsWidget from './widgets/UpcomingCampaignsWidget';
 
 
 export default class Dashboard extends FluxComponent {
@@ -9,7 +10,9 @@ export default class Dashboard extends FluxComponent {
         var i;
         var dashboardStore = this.getStore('dashboard');
         var shortcuts = dashboardStore.getShortcuts();
+        var widgets = dashboardStore.getWidgets();
 
+        var widgetElements = [];
         var favoriteElements = [];
         var shortcutElements = [];
 
@@ -28,6 +31,23 @@ export default class Dashboard extends FluxComponent {
             }
         }
 
+        for (i in widgets) {
+            var WidgetClass = null;
+            var widgetData = widgets[i];
+
+            switch (widgetData.type) {
+                case 'upcoming_campaigns':
+                    WidgetClass = UpcomingCampaignsWidget;
+                    break;
+
+                default:
+                    throw 'Unknown widget type: ' + widgetData.type;
+                    break;
+            }
+
+            widgetElements.push(<WidgetClass key={ i } data={ widgetData }/>);
+        }
+
 
         return (
             <div id="dashboard">
@@ -36,6 +56,9 @@ export default class Dashboard extends FluxComponent {
                 </ul>
                 <ul id="dashboard-shortcuts">
                     { shortcutElements }
+                </ul>
+                <ul id="dashboard-widgets">
+                    { widgetElements }
                 </ul>
             </div>
         );
