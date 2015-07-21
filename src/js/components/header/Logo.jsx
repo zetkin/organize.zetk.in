26e45bd338f90c5c2656297310c1a9080f8bc8dd
Richard Olsson <r@richardolsson.se>
2015-07-21 17:13:49 +0200

    Create Header component for Logo, Search and UserMenu

---
+++ src/js/components/header/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react/addons';
+
+import FluxComponent from '../FluxComponent';
+
+
+export default class Logo extends FluxComponent {
+    render() {
+        return (
+            <h1>Zetkin</h1>
+        );
+    }
+}
---
+++ src/js/components/header/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react/addons';
+
+import FluxComponent from '../FluxComponent';
+
+
+export default class Search extends FluxComponent {
+    render() {
+        return (
+            <form>
+                <input type="search"/>
+            </form>
+        );
+    }
+}
