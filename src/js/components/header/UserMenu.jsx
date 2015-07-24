5f1e6add5eff478a223da6bf0cafe27958e12fc5
Richard Olsson <r@richardolsson.se>
2015-07-24 10:53:59 +0200

    Add organization switcher to user menu

--- src/js/components/header/UserMenu.jsx
+++ src/js/components/header/UserMenu.jsx
@@ -4,13 +4,18 @@ import FluxComponent from '../FluxComponent';
 
 
 export default class UserMenu extends FluxComponent {
+    componentDidMount() {
+        this.listenTo('user', this.forceUpdate);
+    }
+
     render() {
         // TODO: Don't hard-code this
         var accountUrl = 'http://accounts.zetk.in:8000/';
 
         var userStore = this.getStore('user');
         var userName = userStore.getUserInfo().email;
-        var orgName = 'Some org'; // TODO: Selected organization
+        var orgName = userStore.getActiveMembership().organization.title;
+        var memberships = userStore.getMemberships();
 
         return (
             <nav className="user-menu">
@@ -22,8 +27,20 @@ export default class UserMenu extends FluxComponent {
                     <li><a href="/logout">Log out</a></li>
                     <li><a href={ accountUrl }>Account settings</a></li>
                     <li>Switch organization</li>
+                    {memberships.map(function(ms) {
+                        return (
+                            <li onClick={ this.onOrgClick.bind(this, ms) }>
+                                <span>{ ms.organization.title }</span>
+                                <span>{ ms.role }</span>
+                            </li>
+                        );
+                    }, this)}
                 </ul>
             </nav>
         );
+    }
+
+    onOrgClick(membership) {
+        this.getActions('user').setActiveMembership(membership);
     }
 }
