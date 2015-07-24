ecfce5b536c8099d62c18137c8ccc56500b0fa8e
Richard Olsson <r@richardolsson.se>
2015-07-24 12:14:00 +0200

    Render people table in PeopleListPane
    Renders people from store and adds interactivity for opening a person pane when
    clicking a person in the table.

--- src/js/components/sections/people/PeopleListPane.jsx
+++ src/js/components/sections/people/PeopleListPane.jsx
@@ -9,12 +9,34 @@ export default class PeopleListPane extends PaneBase {
         return 'People';
     }
 
+    componentDidMount() {
+        this.listenTo('person', this.forceUpdate);
+        this.getActions('person').getPeople();
+    }
+
     renderPaneContent() {
+        var personStore = this.getStore('person');
+        var people = personStore.getPeople();
+
         return (
             <div>
-                <h3>Jiddermekk</h3>
-                <Link href={ this.subPanePath('person', 1337) }>Person 1337</Link>
+                <h3>People</h3>
+                <table>
+                    {people.map(function(person, index) {
+                        return (
+                            <tr onClick={ this.onPersonClick.bind(this, person) }>
+                                <td>{ person.first_name }</td>
+                                <td>{ person.last_name }</td>
+                                <td>{ person.email }</td>
+                            </tr>
+                        );
+                    }, this)}
+                </table>
             </div>
         );
+    }
+
+    onPersonClick(person) {
+        this.gotoSubPane('person', person.id);
     }
 }
