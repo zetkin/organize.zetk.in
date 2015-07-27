4cc9a17b21fd92bceadd3f83061d051accc9b64b
Richard Olsson <r@richardolsson.se>
2015-07-27 13:55:03 +0200

    Add list view mode to LocationsPane
    Adds a switcher for switching between the default map view mode and a new list
    mode where locations are just listed.

--- src/js/components/sections/maps/LocationsPane.jsx
+++ src/js/components/sections/maps/LocationsPane.jsx
@@ -2,9 +2,18 @@ import React from 'react/addons';
 
 import PaneBase from '../../panes/PaneBase';
 import LocationMap from '../../misc/LocationMap';
+import ViewSwitch from '../../misc/ViewSwitch';
 
 
 export default class LocationsPane extends PaneBase {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            viewMode: 'map'
+        };
+    }
+
     getPaneTitle() {
         return 'Locations';
     }
@@ -15,24 +24,61 @@ export default class LocationsPane extends PaneBase {
     }
 
     renderPaneContent() {
+        var content;
         var locationStore = this.getStore('location');
         var locations = locationStore.getLocations();
 
-        var style = {
-            position: 'absolute',
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
+        var switchStates = {
+            'map': 'Map',
+            'list': 'List'
         };
 
+        if (this.state.viewMode == 'map') {
+            var style = {
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0
+            };
+
+            content = (
+                <LocationMap style={ style} locations={ locations }
+                    onLocationSelect={ this.onLocationSelect.bind(this) }/>
+            );
+        }
+        else if (this.state.viewMode == 'list') {
+            content = (
+                <ul>
+                {locations.map(function(loc) {
+                    return (
+                        <li onClick={ this.onLocationSelect.bind(this, loc) }>
+                            { loc.title }
+                        </li>
+                    );
+                }, this)}
+                </ul>
+            );
+        }
+
         return (
-            <LocationMap style={ style} locations={ locations }
-                onLocationSelect={ this.onLocationSelect.bind(this) }/>
+            <div>
+                <ViewSwitch states={ switchStates }
+                    selected={ this.state.viewMode }
+                    onSwitch={ this.onViewSwitch.bind(this) }/>
+
+                { content }
+            </div>
         );
     }
 
     onLocationSelect(loc) {
         this.gotoSubPane('location', loc.id);
+    }
+
+    onViewSwitch(state) {
+        this.setState({
+            viewMode: state
+        });
     }
 }
