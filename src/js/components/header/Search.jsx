ae8c621f0bdbf438955e2871a1a29cd22740f700
Richard Olsson <r@richardolsson.se>
2015-07-27 18:40:26 +0200

    Fade background when focusing search field

--- src/js/components/header/Search.jsx
+++ src/js/components/header/Search.jsx
@@ -4,11 +4,37 @@ import FluxComponent from '../FluxComponent';
 
 
 export default class Search extends FluxComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            focused: false
+        };
+    }
+
     render() {
+        var classes = ['search-form'];
+        if (this.state.focused) {
+            classes.push('focused');
+        }
         return (
-            <form className="search-form">
-                <input type="search"/>
+            <form className={ classes.join(' ') }>
+                <input type="search"
+                    onFocus={ this.onFocus.bind(this) }
+                    onBlur={ this.onBlur.bind(this) }/>
             </form>
         );
+    }
+
+    onFocus(ev) {
+        this.setState({
+            focused: true
+        });
+    }
+
+    onBlur(ev) {
+        this.setState({
+            focused: false
+        });
     }
 }
