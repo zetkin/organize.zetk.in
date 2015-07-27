5eb20a187e4cf2a52651e49685a15ee7111d3276
Richard Olsson <r@richardolsson.se>
2015-07-27 16:42:35 +0200

    Add LocationForm to LocationPane and implement update

--- src/js/components/panes/LocationPane.jsx
+++ src/js/components/panes/LocationPane.jsx
@@ -1,6 +1,7 @@
 import React from 'react/addons';
 
 import PaneBase from './PaneBase';
+import LocationForm from '../forms/LocationForm';
 
 
 export default class LocationPane extends PaneBase {
@@ -29,11 +30,23 @@ export default class LocationPane extends PaneBase {
     renderPaneContent(data) {
         if (data.loc) {
             // TODO: Render form
-            return data.loc.title;
+            return (
+                <LocationForm ref="form" loc={ data.loc }
+                    onSubmit={ this.onSubmit.bind(this) }/>
+            );
         }
         else {
             // TODO: Show loading indicator?
             return null;
         }
+    }
+
+    onSubmit(ev) {
+        ev.preventDefault();
+
+        var locationId = this.props.params[0];
+        var values = this.refs.form.getChangedValues();
+
+        this.getActions('location').updateLocation(locationId, values);
     }
 }
