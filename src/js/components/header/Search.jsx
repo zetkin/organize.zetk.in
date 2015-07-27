229e33f3bf7e721bbf6203905119eac2e620082f
Richard Olsson <r@richardolsson.se>
2015-07-27 21:02:49 +0200

    Implement search and show results in search form

--- src/js/components/header/Search.jsx
+++ src/js/components/header/Search.jsx
@@ -12,18 +12,53 @@ export default class Search extends FluxComponent {
         };
     }
 
+    componentDidMount() {
+        this.listenTo('search', this.forceUpdate);
+    }
+
     render() {
+        var searchStore = this.getStore('search');
+        var results = searchStore.getResults();
+        var resultList;
         var classes = ['search-form'];
+
         if (this.state.focused) {
             classes.push('focused');
         }
+
+        if (results.length) {
+            resultList = (
+                <ul className="search-form-results">
+                {results.map(function(match) {
+                    var key = match.type + ':' + match.data.id;
+
+                    return (
+                        <li key={ key }>
+                            <span>{ match.type }</span>
+                            <span>{ match.data.id }</span>
+                        </li>
+                    );
+                })}
+                </ul>
+            );
+        }
+
         return (
             <form className={ classes.join(' ') }>
                 <input type="search"
+                    placeholder="Start typing to search"
+                    value={ searchStore.getQuery() }
+                    onChange={ this.onChange.bind(this) }
                     onFocus={ this.onFocus.bind(this) }
                     onBlur={ this.onBlur.bind(this) }/>
+
+                { resultList }
             </form>
         );
+    }
+
+    onChange(ev) {
+        this.getActions('search').search(ev.target.value);
     }
 
     onFocus(ev) {
