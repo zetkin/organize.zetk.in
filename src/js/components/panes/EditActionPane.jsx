d5c38d728c41c434271505b7a207790ebf07a2fe
Richard Olsson <r@richardolsson.se>
2015-07-31 22:29:38 +0200

    Implement ActionForm in EditActionPane

--- src/js/components/panes/EditActionPane.jsx
+++ src/js/components/panes/EditActionPane.jsx
@@ -1,6 +1,7 @@
 import React from 'react/addons';
 
 import PaneBase from './PaneBase';
+import ActionForm from '../forms/ActionForm';
 
 
 export default class EditActionPane extends PaneBase {
@@ -18,21 +19,28 @@ export default class EditActionPane extends PaneBase {
     }
 
     getPaneTitle(data) {
-        if (data.action) {
-            return data.action.id;
-        }
-        else {
-            return null;
-        }
+        return 'Edit action';
     }
 
     renderPaneContent(data) {
         if (data.action) {
-            return data.action.id;
+            return (
+                <ActionForm ref="form" action={ data.action }
+                    onSubmit={ this.onSubmit.bind(this) }/>
+            );
         }
         else {
             // TODO: Show loading indicator?
             return null;
         }
+    }
+
+    onSubmit(ev) {
+        ev.preventDefault();
+
+        var values = this.refs.form.getChangedValues();
+        var actionId = this.props.params[0];
+
+        this.getActions('action').updateAction(actionId, values);
     }
 }
