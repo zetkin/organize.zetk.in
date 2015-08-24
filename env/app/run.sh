596934936c961c429d9eb57a080707165aec6b88
Richard Olsson <r@richardolsson.se>
2015-08-24 15:39:33 +0200

    Prevent npm install when already installed

--- env/app/run.sh
+++ env/app/run.sh
@@ -3,6 +3,9 @@
 cd /var/app
 
 exec 2>&1
-echo "npm install"
-npm install
+if [ ! -d node_modules ]
+then
+    echo "npm install"
+    npm install
+fi
 exec node dist/organize.zetk.in/server/main.js
