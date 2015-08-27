ec227066d7ad1bd5f2650f449c7ad4ed7e068bcf
Richard Olsson <r@richardolsson.se>
2015-08-27 19:18:31 +0200

    Create ActionLocations component
    Renders a list of locations from an array of actions, along with the number
    of actions for each location. It also renders a ball, sized relatively to the
    average actions per location to indicate the relative frequencies.

---
+++ src/js/components/misc/actionlocations/ActionLocationItem.jsx
@@ -0,0 +1,41 @@
+import React from 'react/addons';
+
+export default class ActionLocationItem extends React.Component {
+    render() {
+        const loc = this.props.location;
+        const counts = [
+            this.props.numMorningActions,
+            this.props.numNoonActions,
+            this.props.numAfternoonActions,
+            this.props.numEveningActions,
+            this.props.numNightActions
+        ];
+
+        const totalCount = counts.reduce((x, y) => x + y, 0);
+        const average = this.props.numActionsAverage;
+        const ballSize = Math.max(24, Math.min(50, 37 * totalCount/average));
+        const countStyle = {
+            width: ballSize,
+            height: ballSize
+        };
+
+        return (
+            <li>
+                <span className="title">{ loc.title }</span>
+                <span style={ countStyle } className="actioncount">
+                    <span>{ totalCount }</span></span>
+            </li>
+        );
+    }
+}
+
+ActionLocationItem.propTypes = {
+    numMorningActions: React.PropTypes.number.isRequired,
+    numNoonActions: React.PropTypes.number.isRequired,
+    numAfternoonActions: React.PropTypes.number.isRequired,
+    numEveningActions: React.PropTypes.number.isRequired,
+    numNightActions: React.PropTypes.number.isRequired,
+    location: React.PropTypes.shape({
+        title: React.PropTypes.string.isRequired
+    }).isRequired
+};
