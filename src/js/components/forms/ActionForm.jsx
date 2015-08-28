f7b9c08248a14100b74f0d90cba80e03a0632d04
Richard Olsson <r@richardolsson.se>
2015-08-28 19:10:17 +0200

    Split date and start/end times in separate fields
    This is really hacky, and will need to be solved in some nicer, more
    general-purpose way.

--- src/js/components/forms/ActionForm.jsx
+++ src/js/components/forms/ActionForm.jsx
@@ -2,8 +2,9 @@ import React from 'react/addons';
 
 import FluxComponent from '../FluxComponent';
 import Form from './Form';
+import DateInput from './inputs/DateInput';
 import TextArea from './inputs/TextArea';
-import TextInput from './inputs/TextInput';
+import TimeInput from './inputs/TimeInput';
 import RelSelectInput from './inputs/RelSelectInput';
 
 
@@ -28,15 +29,22 @@ export default class ActionForm extends FluxComponent {
         const locations = this.getStore('location').getLocations();
         const activities = this.getStore('activity').getActivities();
 
+        const date = action.start_time? new Date(action.start_time) : undefined;
+        const startTime = date? formatTime(date) : undefined;
+        const endTime = action.end_time?
+            formatTime(new Date(action.end_time)) : undefined;
+
         return (
             <Form ref="form" {...this.props }>
                 <RelSelectInput label="Campaign" name="campaign_id"
                     objects={ campaigns }
                     initialValue={ action.campaign.id }/>
-                <TextInput label="Start" name="start_time"
-                    initialValue={ action.start_time }/>
-                <TextInput label="End" name="end_time"
-                    initialValue={ action.end_time }/>
+                <DateInput label="Date" name="date"
+                    initialValue={ date }/>
+                <TimeInput label="Start" name="start_time"
+                    initialValue={ startTime }/>
+                <TimeInput label="End" name="end_time"
+                    initialValue={ endTime }/>
                 <RelSelectInput label="Location" name="location_id"
                     objects={ locations }
                     initialValue={ action.location.id }/>
@@ -52,10 +60,60 @@ export default class ActionForm extends FluxComponent {
     }
 
     getValues() {
-        return this.refs.form.getValues();
+        const values = this.refs.form.getValues();
+        const date = new Date(values.date);
+
+        updateTime(values, 'start_time', date);
+        updateTime(values, 'end_time', date);
+        delete values.date;
+
+        return values;
     }
 
     getChangedValues() {
-        return this.refs.form.getChangedValues();
+        const values = this.refs.form.getChangedValues();
+        const date = new Date(this.refs.form.getValues().date);
+
+        if ('date' in values) {
+            const allValues = this.getValues();
+            values.start_time = allValues.start_time;
+            values.end_time = allValues.end_time;
+
+            delete values.date;
+        }
+        else {
+            if ('start_time' in values) {
+                updateTime(values, 'start_time', date);
+            }
+            if ('end_time' in values) {
+                updateTime(values, 'end_time', date);
+            }
+        }
+
+        return values;
     }
+}
+
+function pad(n) {
+    return (n<10)? '0' + n : n.toString();
+}
+
+function formatTime(d) {
+    return pad(d.getUTCHours()) + ':'
+        + pad(d.getUTCMinutes());
+}
+
+function updateTime(values, field, date) {
+    const val = values[field];
+
+    const fields = val.split(':');
+    const hours = parseInt(fields[0]);
+    const minutes = parseInt(fields[1]);
+    const tmp = new Date(date);
+
+    tmp.setHours(hours);
+    tmp.setMinutes(minutes);
+    tmp.setSeconds(0);
+
+    values[field] = tmp.toISOString();
 }
