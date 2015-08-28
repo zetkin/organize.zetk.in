ac07b719aa0cb34385189c78b6e54d4851854d34
Richard Olsson <r@richardolsson.se>
2015-08-28 14:17:24 +0200

    Add crude ActionMiniCalendar to CampaignLocationsPane
    Based on the regular ActionCalendar, using the same ActionDay component to
    automatically support drag and drop.

---
+++ src/js/components/misc/actioncal/ActionMiniCalendar.jsx
@@ -0,0 +1,66 @@
+import React from 'react/addons';
+
+import ActionDay from './ActionDay';
+
+
+export default class ActionMiniCalendar extends React.Component {
+    render() {
+        const actions = this.props.actions;
+
+        var startDate = this.props.startDate;
+        var endDate = this.props.endDate;
+
+        if (!startDate && actions.length > 0) {
+            startDate = new Date(actions[0].start_time);
+        }
+
+        if (!endDate && actions.length > 0) {
+            endDate = new Date(actions[actions.length-1].end_time);
+        }
+
+        var d = new Date(startDate.toDateString());
+        var days = [];
+        var idx = 0;
+
+        while (d <= endDate) {
+            var dayActions = [];
+
+            while (idx < actions.length) {
+                const action = actions[idx];
+                const ad = new Date(action.start_time);
+
+                if (d.getYear() == ad.getYear()
+                    && d.getMonth() == ad.getMonth()
+                    && d.getDate() == ad.getDate()) {
+
+                    dayActions.push(action);
+
+                    idx++;
+                }
+                else {
+                    break;
+                }
+            }
+
+            days.push(
+                <ActionDay date={ new Date(d) } actions={ dayActions }
+                    onMoveAction={ this.onMoveAction.bind(this) }
+                    onSelectAction={ this.onSelectAction.bind(this) }/>
+            );
+
+            d.setDate(d.getDate() + 1);
+        }
+
+        return (
+            <div className="actionminicalendar">
+                { days }
+            </div>
+        );
+    }
+
+    onMoveAction(action, date) {
+    }
+
+    onSelectAction(action) {
+    }
+}
--- src/js/components/sections/campaign/CampaignLocationsPane.jsx
+++ src/js/components/sections/campaign/CampaignLocationsPane.jsx
@@ -3,6 +3,7 @@ import React from 'react/addons';
 import PaneBase from '../../panes/PaneBase';
 import CampaignSelect from '../../misc/CampaignSelect';
 import ActionLocations from '../../misc/actionlocations/ActionLocations';
+import ActionMiniCalendar from '../../misc/actioncal/ActionMiniCalendar';
 
 
 export default class AllActionsPane extends PaneBase {
@@ -14,6 +15,13 @@ export default class AllActionsPane extends PaneBase {
         this.listenTo('action', this.forceUpdate);
         this.listenTo('campaign', this.forceUpdate);
         this.getActions('action').retrieveAllActions();
+    }
+
+    renderPaneTop() {
+        const actionStore = this.getStore('action');
+        const actions = actionStore.getActions();
+
+        return <ActionMiniCalendar actions={ actions }/>
     }
 
     renderPaneContent() {
