16d89b58075ba8bdb3696df011dbbeda78d80db8
Richard Olsson <r@richardolsson.se>
2015-08-29 10:17:22 +0200

    Implement input parameters to AddActionPane
    First parameter is campaign ID, second is date. When clicking add in a calendar
    day, the date will be passed. If a campaign is selected, that campaign will be
    passed along as well. These allow sensible values to be pre-filled when the
    user creates a new action.

--- src/js/components/panes/AddActionPane.jsx
+++ src/js/components/panes/AddActionPane.jsx
@@ -10,8 +10,27 @@ export default class AddActionPane extends PaneBase {
     }
 
     renderPaneContent(data) {
+        const initialData = {
+            campaign: {
+                id: this.getParam(0, 0)
+            },
+            location: { id: 0 },
+            activity: { id: 0 }
+        };
+
+        const dateParam = this.getParam(1);
+        if (dateParam) {
+            const date = new Date(dateParam);
+
+            date.setHours(9);
+            initialData.start_time = date.toISOString();
+
+            date.setHours(11);
+            initialData.end_time = date.toISOString();
+        }
+
         return (
-            <ActionForm ref="actionForm"
+            <ActionForm ref="actionForm" action={ initialData }
                 onSubmit={ this.onSubmit.bind(this) }/>
         );
     }
