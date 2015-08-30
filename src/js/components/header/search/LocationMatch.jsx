d010a5127149f077ec17f1090d0dab1156eaf8ea
Richard Olsson <r@richardolsson.se>
2015-08-30 15:41:20 +0200

    Remove dead getLinkTarget() functions

--- src/js/components/header/search/LocationMatch.jsx
+++ src/js/components/header/search/LocationMatch.jsx
@@ -5,10 +5,6 @@ import MatchBase from './MatchBase';
 
 
 export default class LocationMatch extends MatchBase {
-    getLinkTarget() {
-        return '/maps/locations/location:' + this.props.data.id;
-    }
-
     getTitle() {
         return this.props.data.title;
     }
