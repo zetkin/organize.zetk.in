b7a33740370c8eead162e27a736229073306f2c5
Richard Olsson <r@richardolsson.se>
2015-08-30 15:40:30 +0200

    Add images to person search matches

--- src/js/components/header/search/PersonMatch.jsx
+++ src/js/components/header/search/PersonMatch.jsx
@@ -1,14 +1,15 @@
 import React from 'react/addons';
 
 import MatchBase from './MatchBase';
+import Avatar from '../../misc/Avatar';
 
 
 export default class PersonMatch extends MatchBase {
-    getLinkTarget() {
-        return '/people/list/person:' + this.props.data.id;
-    }
-
     getTitle() {
         return this.props.data.first_name + ' ' + this.props.data.last_name;
+    }
+
+    getImage() {
+        return <Avatar person={ this.props.data }/>;
     }
 }
