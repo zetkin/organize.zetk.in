9eb703125533ce08514455a36ba83af323690d78
Richard Olsson <r@richardolsson.se>
2015-08-30 19:06:06 +0200

    Show only highlight count when overflow contains highlights

--- src/js/components/misc/actioncal/ActionDayOverflow.jsx
+++ src/js/components/misc/actioncal/ActionDayOverflow.jsx
@@ -1,15 +1,24 @@
 import React from 'react/addons';
+import cx from 'classnames';
 
 
 export default class ActionDayOverflow extends React.Component {
     render() {
         const actions = this.props.actions;
         const actionCount = actions.length;
+        const highlightActions = actions.filter(a => a.highlight);
+        const highlightCount = highlightActions.length;
+
+        const label = highlightCount || actionCount;
+
+        const classes = cx({
+            'actionday-overflow': true,
+            'highlight': (highlightCount > 0)
+        });
 
         return (
-            <li className="actionday-pseudoitem actionday-overflow"
-                onClick={ this.props.onClick }>
-                { actionCount }
+            <li className={ classes } onClick={ this.props.onClick }>
+                { label }
             </li>
         );
     }
