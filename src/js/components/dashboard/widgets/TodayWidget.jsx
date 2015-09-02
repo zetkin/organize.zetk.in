041b2200ff84dabafc0d73fa7b7345407b920e31
Richard Olsson <r@richardolsson.se>
2015-09-02 09:49:52 +0200

    Create crude TodayWidget
    * Shows weather information from OpenWeatherMap
    * Shows simple statistics about today's actions

--- src/js/components/dashboard/widgets/TodayWidget.jsx
+++ src/js/components/dashboard/widgets/TodayWidget.jsx
@@ -4,7 +4,35 @@ import WidgetBase from './WidgetBase';
 
 
 export default class TodayWidget extends WidgetBase {
-    renderWidget() {
-        return <h1>Today</h1>;
+    renderWidget(data) {
+        const tempLabel = Math.round(data.weather.temp) + '°C';
+        const weatherIcon = 'http://openweathermap.org/img/w/'
+            .concat(data.weather.forecast.icon + '.png');
+
+        const locations = {};
+        for (let i = 0; i < data.actions.length; i++) {
+            let action = data.actions[i];
+            let locationId = action.location.id;
+            if (!locations.hasOwnProperty(locationId)) {
+                locations[locationId] = action.location;
+            }
+        }
+
+        const locationCount = Object.keys(locations).length;
+        const actionCount = data.actions.length;
+
+        return [
+            <div className="weather">
+                <img src={ weatherIcon }/>
+                <span className="temperature">{ tempLabel }</span>
+                <span className="city">{ data.weather.city }</span>
+            </div>,
+            <ul className="stats">
+                <li><span className="label">Actions today:</span>
+                    <span className="value">{ actionCount }</span></li>
+                <li><span className="label">Locations visited:</span>
+                    <span className="value">{ locationCount }</span></li>
+            </ul>
+        ];
     }
 }
