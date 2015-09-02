c6fdc740ad5a9c02883488840a7c4651f8e33174
Richard Olsson <r@richardolsson.se>
2015-09-02 16:57:23 +0200

    Add query select to PeopleListPane
    Selecting a query executes it against the person list, filtering it according
    to the query filter spec.

--- src/js/components/sections/people/PeopleListPane.jsx
+++ src/js/components/sections/people/PeopleListPane.jsx
@@ -5,11 +5,20 @@ import PeopleList from '../../misc/peoplelist/PeopleList';
 
 
 export default class PeopleListPane extends PaneBase {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedQueryId: undefined
+        };
+    }
+
     getPaneTitle() {
         return 'People';
     }
 
     componentDidMount() {
+        this.listenTo('query', this.forceUpdate);
         this.listenTo('person', this.forceUpdate);
         this.getActions('person').retrievePeople();
     }
@@ -18,10 +27,29 @@ export default class PeopleListPane extends PaneBase {
         var personStore = this.getStore('person');
         var people = personStore.getPeople();
 
+        const queryId = this.state.selectedQueryId;
+        const queryStore = this.getStore('query');
+        const queries = queryStore.getQueries();
+
+        if (queryId) {
+            people = queryStore.executeQuery(queryId, people);
+        }
+
         return [
-            <input type="button" value="Add"
+            <input key="addButton" type="button" value="Add"
                 onClick={ this.onAddClick.bind(this) }/>,
-            <PeopleList people={ people }
+            <select key="querySelect" value={ queryId }
+                onChange={ this.onQueryChange.bind(this) }>
+                <option value="">----</option>
+                {queries.map(function(query) {
+                    return (
+                        <option key={ query.id } value={ query.id }>
+                            { query.title }
+                        </option>
+                    );
+                })}
+            </select>,
+            <PeopleList key="peopleList" people={ people }
                 onSelect={ this.onSelect.bind(this) }/>
         ];
     }
@@ -32,5 +60,11 @@ export default class PeopleListPane extends PaneBase {
 
     onAddClick(person) {
         this.gotoSubPane('addperson');
+    }
+
+    onQueryChange(ev) {
+        this.setState({
+            selectedQueryId: ev.target.value
+        });
     }
 }
