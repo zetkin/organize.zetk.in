0571f6c4dc234cfb04110c7733e3f95a38dfad96
Richard Olsson <r@richardolsson.se>
2015-09-03 10:04:17 +0200

    Correct (internal) class name of ActionDayMatch

--- src/js/components/header/search/ActionDayMatch.jsx
+++ src/js/components/header/search/ActionDayMatch.jsx
@@ -4,7 +4,7 @@ import url from 'url';
 import MatchBase from './MatchBase';
 
 
-export default class LocationMatch extends MatchBase {
+export default class ActionDayMatch extends MatchBase {
     getTitle() {
         const count = this.props.data.action_count;
         const date = this.props.data.date;
