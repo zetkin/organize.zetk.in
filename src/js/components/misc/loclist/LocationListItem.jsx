0a8214ba15b7f0b28de3887af847ce9f76c3ff59
Richard Olsson <r@richardolsson.se>
2015-09-08 14:54:43 +0200

    Create simple LocationList component

---
+++ src/js/components/misc/loclist/LocationListItem.jsx
@@ -0,0 +1,47 @@
+import React from 'react/addons';
+import url from 'url';
+
+
+export default class LocationListItem extends React.Component {
+    render() {
+        const loc = this.props.location;
+        const lat = loc.lat;
+        const lng = loc.lng;
+
+        const imgSrc = url.format({
+            protocol: 'https',
+            hostname: 'maps.googleapis.com',
+            pathname: '/maps/api/staticmap',
+            query: {
+                center: (lat + 0.0002) + ',' + lng,
+                markers: lat + ',' + lng,
+                zoom: 15,
+                size: '120x90'
+            }
+        });
+
+        return (
+            <li className="locationlistitem"
+                onClick={ this.onClick.bind(this) }>
+                <img src={ imgSrc }/>
+                <span className="title">{ loc.title }</span>
+            </li>
+        );
+    }
+
+    onClick() {
+        if (this.props.onSelect) {
+            this.props.onSelect(this.props.location);
+        }
+    }
+}
+
+LocationListItem.propTypes = {
+    onSelect: React.PropTypes.func,
+    location: React.PropTypes.shape({
+        id: React.PropTypes.number.isRequired,
+        title: React.PropTypes.string.isRequired,
+        lat: React.PropTypes.number.isRequired,
+        lng: React.PropTypes.number.isRequired
+    }).isRequired
+};
