919411d8c3edf5ee09355fa7af17c06e582af627
Richard Olsson <r@richardolsson.se>
2015-09-23 13:00:48 +0200

    Use componentClassNames() in WidgetBase
    Automatically collects both the `WidgetBase` class name and the name of the
    concrete component class that extends `WidgetBase`.

--- src/js/components/dashboard/widgets/WidgetBase.jsx
+++ src/js/components/dashboard/widgets/WidgetBase.jsx
@@ -1,6 +1,8 @@
 import React from 'react/addons';
+import cx from 'classnames';
 
 import FluxComponent from '../../FluxComponent';
+import { componentClassNames } from '../../';
 
 
 export default class WidgetBase extends FluxComponent {
@@ -16,7 +18,7 @@ export default class WidgetBase extends FluxComponent {
 
     render() {
         const type = this.props.config.type;
-        const classes = 'WidgetBase-widget WidgetBase-widget-' + type;
+        const classes = cx(componentClassNames(this));
 
         const store = this.getStore('dashboard');
         const data = store.getWidgetData(type);
