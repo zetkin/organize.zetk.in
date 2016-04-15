2f9ee349a2e66775fc56cb4ac033da86c3e2fb3f
Richard Olsson <r@richardolsson.se>
2016-04-15 20:27:19 +0200

    Move global styling to _global.scss
    A lot of the SCSS in this file should eventually be removed or moved elsewhere.

--- gulpfile.js
+++ gulpfile.js
@@ -78,10 +78,14 @@ gulp.task('bundleJs', [ 'js' ], function(cb) {
 
 gulp.task('buildSass', [ 'cleanSass' ], function() {
     return gulp.src([
+            'src/scss/_mixins.scss',
             'src/scss/_variables.scss',
             'src/scss/_helpers.scss',
+            'src/scss/font-awesome/zetkin-font-awesome.scss',
             'src/scss/_global.scss',
-            'src/!(scss)/**/*.scss'
+            'src/scss/_base.scss',
+            'src/scss/_medium.scss',
+            'src/!(scss)/**/*.scss',
         ])
         .pipe(concat('style.scss'))
         .pipe(sass())
