776e67c81631451224bffe5de07c7c010b6346bc
Richard Olsson <r@richardolsson.se>
2016-05-12 13:18:28 +0200

    Add makeRandomString utility function

---
+++ src/utils/makeRandomString.js
@@ -0,0 +1,12 @@
+const ALLOWED_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+export default function makeRandomString(len) {
+    let str = '';
+
+    for (let i = 0; i < len; i++) {
+        let idx = Math.floor(Math.random() * ALLOWED_CHARS.length);
+        str += ALLOWED_CHARS.charAt(idx);
+    }
+
+    return str;
+}
