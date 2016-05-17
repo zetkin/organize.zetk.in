08e7a47d8a03b1820c4628819b8edc82ff94f9a9
Richard Olsson <r@richardolsson.se>
2016-05-17 10:17:29 +0200

    Add crude target section to CallAssignmentPane

--- src/components/panes/CallAssignmentPane.jsx
+++ src/components/panes/CallAssignmentPane.jsx
@@ -5,16 +5,37 @@ import PaneBase from './PaneBase';
 import { getListItemById } from '../../utils/store';
 import { createTextDocument } from '../../actions/document';
 import { updateCallAssignment } from '../../actions/callAssignment';
+import { retrieveQueryMatches } from '../../actions/query';
 
 
 @connect(state => state)
 export default class CallAssignmentPane extends PaneBase {
+    componentDidMount() {
+        let assignmentId = this.getParam(0);
+        let assignmentList = this.props.callAssignments.assignmentList;
+        let assignmentItem = getListItemById(assignmentList, assignmentId);
+
+        if (assignmentItem) {
+            let queryId = assignmentItem.data.target.id;
+            this.props.dispatch(retrieveQueryMatches(queryId));
+        }
+    }
+
     getRenderData() {
         let assignmentId = this.getParam(0);
         let assignmentList = this.props.callAssignments.assignmentList;
+        let assignmentItem = getListItemById(assignmentList, assignmentId);
+
+        let queryItem = null;
+        if (assignmentItem) {
+            let queryList = this.props.queries.queryList;
+            let queryId = assignmentItem.data.target.id;
+            queryItem = getListItemById(queryList, queryId);
+        }
 
         return {
-            assignmentItem: getListItemById(assignmentList, assignmentId),
+            assignmentItem: assignmentItem,
+            queryItem: queryItem,
         };
     }
 
@@ -32,6 +53,11 @@ export default class CallAssignmentPane extends PaneBase {
         if (data.assignmentItem) {
             let assignment = data.assignmentItem.data;
             let instructions = assignment.instructions;
+
+            let targetContent = null;
+            if (data.queryItem && data.queryItem.data.matchList) {
+                targetContent = <h1>{ data.queryItem.data.matchList.items.length }</h1>;
+            }
 
             return [
                 <div key="summary"
@@ -51,6 +77,11 @@ export default class CallAssignmentPane extends PaneBase {
                 <div key="target"
                     className="CallAssignmentPane-target">
                     <h3>Targets</h3>
+                    <div>
+                        { targetContent }
+                    </div>
+                    <a onClick={ this.onClickEditTarget.bind(this) }>
+                        Edit target filters</a>
                 </div>,
 
                 <div key="callers"
@@ -62,6 +93,14 @@ export default class CallAssignmentPane extends PaneBase {
         else {
             return 'Loading';
         }
+    }
+
+    onClickEditTarget(ev) {
+        let assignmentId = this.getParam(0);
+        let assignmentList = this.props.callAssignments.assignmentList;
+        let assignmentItem = getListItemById(assignmentList, assignmentId);
+
+        this.openPane('editquery', assignmentItem.data.target.id);
     }
 
     onClickEditInstructions(ev) {
