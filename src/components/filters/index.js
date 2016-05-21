53cf96b75969de1fa49ce07b0bbc7924da967a04
Richard Olsson <r@richardolsson.se>
2016-05-21 15:42:32 +0200

    Implement CallHistoryFilter (similar to CampaignFilter)

--- src/components/filters/index.js
+++ src/components/filters/index.js
@@ -1,8 +1,10 @@
+import CallHistoryFilter from './CallHistoryFilter';
 import CampaignFilter from './CampaignFilter';
 import JoinDateFilter from './JoinDateFilter';
 import PersonDataFilter from './PersonDataFilter';
 
 const filterComponents = {
+    'call_history': CallHistoryFilter,
     'campaign_participation': CampaignFilter,
     'join_date': JoinDateFilter,
     'person_data': PersonDataFilter
