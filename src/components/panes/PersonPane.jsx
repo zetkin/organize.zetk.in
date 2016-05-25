82bd804be40422fca9f89692fd1ed83192635a9c
Richard Olsson <r@richardolsson.se>
2016-05-25 17:02:07 +0200

    Add tag cloud to PersonPane

--- src/components/panes/PersonPane.jsx
+++ src/components/panes/PersonPane.jsx
@@ -3,8 +3,15 @@ import { connect } from 'react-redux';
 
 import PaneBase from './PaneBase';
 import DraggableAvatar from '../misc/DraggableAvatar';
+import TagCloud from '../misc/tagcloud/TagCloud';
 import { getListItemById } from '../../utils/store';
 import { retrievePerson } from '../../actions/person';
+import { createSelection } from '../../actions/selection';
+import {
+    addTagsToPerson,
+    removeTagFromPerson,
+    retrieveTagsForPerson,
+} from '../../actions/personTag';
 
 
 const FIELDS = {
@@ -16,11 +23,12 @@ const FIELDS = {
     'city': 'City',
 };
 
-@connect(state => ({ people: state.people }))
+@connect(state => ({ people: state.people, personTags: state.personTags }))
 export default class PersonPane extends PaneBase {
     componentDidMount() {
         let personId = this.getParam(0);
         this.props.dispatch(retrievePerson(personId));
+        this.props.dispatch(retrieveTagsForPerson(personId));
     }
 
     getRenderData() {
@@ -46,6 +54,22 @@ export default class PersonPane extends PaneBase {
         if (data.personItem) {
             let person = data.personItem.data;
 
+            let tagCloud = null;
+            if (person.tagList && !person.tagList.isPending) {
+                let tagList = this.props.personTags.tagList;
+                let tags = person.tagList.items
+                    .map(i => getListItemById(tagList, i.data.id))
+                    .filter(i => i !== null)
+                    .map(i => i.data);
+
+                tagCloud = (
+                    <TagCloud tags={ tags }
+                        showAddButton={ true } showRemoveButtons={ true }
+                        onRemove={ this.onRemoveTag.bind(this) }
+                        onAdd={ this.onAddTag.bind(this) }/>
+                );
+            }
+
             return [
                 <DraggableAvatar key="avatar" ref="avatar" person={ person }/>,
                 <ul key="info" className="PersonPane-info">
@@ -63,12 +87,33 @@ export default class PersonPane extends PaneBase {
                 </ul>,
                 <a onClick={ this.onClickEdit.bind(this) }>
                     Edit basic information</a>,
+                <div className="PersonPane-tags">
+                    <h3>Tags</h3>
+                    { tagCloud }
+                </div>,
             ];
         }
         else {
             // TODO: Loading indicator
             return null;
         }
+    }
+
+    onAddTag() {
+        let personId = this.getParam(0);
+        let action = createSelection('persontag', null, null, ids => {
+            // TODO: Execute action
+            this.props.dispatch(addTagsToPerson(personId, ids));
+        });
+
+        this.props.dispatch(action);
+        this.openPane('selectpersontags', action.payload.id);
+    }
+
+
+    onRemoveTag(tag) {
+        let personId = this.getParam(0);
+        this.props.dispatch(removeTagFromPerson(personId, tag.id));
     }
 
     onClickEdit(ev) {
