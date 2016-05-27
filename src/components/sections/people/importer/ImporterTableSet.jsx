ce7f1d669d5b44159f15f0fc0f8b7f15112e8f5f
Richard Olsson <r@richardolsson.se>
2016-05-27 10:30:16 +0200

    Implement crude switching between imported tables

--- src/components/sections/people/importer/ImporterTableSet.jsx
+++ src/components/sections/people/importer/ImporterTableSet.jsx
@@ -8,15 +8,48 @@ export default class ImporterTableSet extends React.Component {
         tableSet: React.PropTypes.object.isRequired,
     };
 
+    constructor(props) {
+        super(props);
+
+        let tableSet = props.tableSet;
+        if (tableSet && tableSet.tables && tableSet.tables.length > 0) {
+            this.state = {
+                selectedTableId: tableSet.tables[0].id,
+            };
+        }
+    }
+
     render() {
         let tableSet = this.props.tableSet;
 
+        let table = null;
+        if (this.state.selectedTableId) {
+            let tableData = tableSet.tables.find(table =>
+                table.id == this.state.selectedTableId);
+
+            table = (
+                <ImporterTable key={ tableData.id } table={ tableData }/>
+            );
+        }
+
         return (
             <div className="ImporterTableSet">
-            { tableSet.tables.map((table, idx) => (
-                <ImporterTable key={ idx } table={ table }/>
-            )) }
+                <ul className="ImporterTableSet-tabs">
+                { tableSet.tables.map(table => (
+                    <li key={ table.id } className="ImporterTableSet-tab"
+                        onClick={ this.onClickTab.bind(this, table) }>
+                        { table.name }
+                    </li>
+                )) }
+                </ul>
+                { table }
             </div>
         );
+    }
+
+    onClickTab(table) {
+        this.setState({
+            selectedTableId: table.id,
+        });
     }
 }
