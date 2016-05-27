6577aa1d44730c48323af1857dfea51722d108a4
Jens Börjesson <jens@dreamsinteractive.se>
2016-05-27 13:54:03 +0200

    Moved all styling to scss for LoadingIndicator

--- src/components/misc/LoadingIndicator.jsx
+++ src/components/misc/LoadingIndicator.jsx
@@ -3,26 +3,15 @@ import React from 'react';
 
 export default class LoadingIndicator extends React.Component {
 
-    static propTypes = {
-        height: React.PropTypes.number.isRequired
-    };
-
-    static defaultProps = {
-        height: 100
-    };
-
-
-
+    
     render() {
         return (
-            <span style={{ height: this.props.height }} 
-                className="LoadingIndicator">
-                <span style={{ paddingTop: this.props.height / 2 - 16 }}
-                className="LoadingIndicator-icon"/>
+            <span className="LoadingIndicator">
+                <span className="LoadingIndicator-icon"/>
             </span>
         );
     }
 
-    
+
 
 }
