a4fd165b303cf6e120ef2e4631372484f2fbb2dc
Richard Olsson <r@richardolsson.se>
2016-09-08 16:13:22 +0200

    Migrate action actions to new action scheme

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,54 +1,52 @@
-import Z from 'zetkin';
-
 import * as types from '.';
 
 
 export function retrieveActions() {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.RETRIEVE_ACTIONS,
             payload: {
-                promise: Z.resource('orgs', orgId, 'actions').get()
+                promise: z.resource('orgs', orgId, 'actions').get()
             }
         });
     };
 }
 
 export function retrieveAction(id) {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.RETRIEVE_ACTION,
             meta: { id },
             payload: {
-                promise: Z.resource('orgs', orgId, 'actions', id).get()
+                promise: z.resource('orgs', orgId, 'actions', id).get()
             }
         });
     };
 }
 
 export function updateAction(id, data) {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.UPDATE_ACTION,
             meta: { id },
             payload: {
-                promise: Z.resource('orgs', orgId, 'actions', id).patch(data)
+                promise: z.resource('orgs', orgId, 'actions', id).patch(data)
             }
         });
     };
 }
 
 export function createAction(campaignId, data) {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.CREATE_ACTION,
             meta: { campaignId },
             payload: {
-                promise: Z.resource('orgs', orgId,
+                promise: z.resource('orgs', orgId,
                     'campaigns', campaignId, 'actions').post(data)
             }
         });
@@ -56,13 +54,13 @@ export function createAction(campaignId, data) {
 }
 
 export function sendActionReminders(actionId) {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.SEND_ACTION_REMINDERS,
             meta: { actionId },
             payload: {
-                promise: Z.resource('orgs', orgId,
+                promise: z.resource('orgs', orgId,
                     'actions', actionId, 'reminders').post()
             }
         });
