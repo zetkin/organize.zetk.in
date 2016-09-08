f93b9ea7e801258aca6dcb4c87154661cd74e8c7
Richard Olsson <r@richardolsson.se>
2016-09-08 16:11:45 +0200

    Migrate campaign actions to new action scheme

--- src/actions/campaign.js
+++ src/actions/campaign.js
@@ -1,66 +1,64 @@
-import Z from 'zetkin';
-
 import * as types from './';
 
 
 export function createCampaign(data) {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.CREATE_CAMPAIGN,
             payload: {
-                promise: Z.resource('orgs', orgId, 'campaigns').post(data),
+                promise: z.resource('orgs', orgId, 'campaigns').post(data),
             }
         });
     };
 }
 
 export function retrieveCampaigns() {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.RETRIEVE_CAMPAIGNS,
             payload: {
-                promise: Z.resource('orgs', orgId, 'campaigns').get(),
+                promise: z.resource('orgs', orgId, 'campaigns').get(),
             }
         });
     };
 }
 
 export function retrieveCampaign(id) {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.RETRIEVE_CAMPAIGN,
             meta: { id },
             payload: {
-                promise: Z.resource('orgs', orgId, 'campaigns', id).get(),
+                promise: z.resource('orgs', orgId, 'campaigns', id).get(),
             }
         });
     };
 }
 
 export function updateCampaign(id, data) {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.UPDATE_CAMPAIGN,
             meta: { id },
             payload: {
-                promise: Z.resource('orgs', orgId, 'campaigns', id).patch(data),
+                promise: z.resource('orgs', orgId, 'campaigns', id).patch(data),
             }
         });
     };
 }
 
 export function deleteCampaign(id) {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.DELETE_CAMPAIGN,
             meta: { id },
             payload: {
-                promise: Z.resource('orgs', orgId, 'campaigns', id).del(),
+                promise: z.resource('orgs', orgId, 'campaigns', id).del(),
             }
         });
     };
