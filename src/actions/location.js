6664b1f14a11471a372089ee228b359be16ee55a
Richard Olsson <r@richardolsson.se>
2016-09-08 16:16:49 +0200

    Migrate activity and location actions to new action scheme

--- src/actions/location.js
+++ src/actions/location.js
@@ -1,66 +1,64 @@
-import Z from 'zetkin';
-
 import * as types from './';
 
 
 export function createLocation(data) {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.CREATE_LOCATION,
             payload: {
-                promise: Z.resource('orgs', orgId, 'locations').post(data),
+                promise: z.resource('orgs', orgId, 'locations').post(data),
             }
         });
     };
 }
 
 export function retrieveLocations() {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.RETRIEVE_LOCATIONS,
             payload: {
-                promise: Z.resource('orgs', orgId, 'locations').get(),
+                promise: z.resource('orgs', orgId, 'locations').get(),
             }
         });
     };
 }
 
 export function retrieveLocation(id) {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.RETRIEVE_LOCATION,
             meta: { id },
             payload: {
-                promise: Z.resource('orgs', orgId, 'locations', id).get(),
+                promise: z.resource('orgs', orgId, 'locations', id).get(),
             }
         });
     };
 }
 
 export function updateLocation(id, data) {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.UPDATE_LOCATION,
             meta: { id },
             payload: {
-                promise: Z.resource('orgs', orgId, 'locations', id).patch(data),
+                promise: z.resource('orgs', orgId, 'locations', id).patch(data),
             }
         });
     };
 }
 
 export function deleteLocation(id) {
-    return function(dispatch, getState) {
+    return ({ dispatch, getState, z }) => {
         let orgId = getState().org.activeId;
         dispatch({
             type: types.DELETE_LOCATION,
             meta: { id },
             payload: {
-                promise: Z.resource('orgs', orgId, 'locations', id).del(),
+                promise: z.resource('orgs', orgId, 'locations', id).del(),
             }
         });
     };
