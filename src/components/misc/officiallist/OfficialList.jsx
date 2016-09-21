6d3f6b93be82a1ad213b9a67f38609df8ecad8ef
Richard Olsson <r@richardolsson.se>
2016-09-21 11:04:56 +0200

    Add components to render lists of officials
    Based on caller list in CallAssignmentPane

---
+++ src/components/misc/officiallist/OfficialList.jsx
@@ -0,0 +1,76 @@
+import cx from 'classnames';
+import React from 'react';
+import { DropTarget } from 'react-dnd';
+
+import OfficialListItem from './OfficialListItem';
+
+
+const officialTarget = {
+    canDrop(props, monitor) {
+        let person = monitor.getItem();
+        let officials = props.officials;
+        let duplicate = officials.find(p => (p.id == person.id));
+
+        // Only allow drops if it wouldn't result in duplicate
+        return (duplicate === undefined);
+    },
+
+    drop(props) {
+        return {
+            targetType: 'official',
+            onDropPerson: p => props.onAdd(p)
+        };
+    }
+};
+
+function collectOfficial(connect, monitor) {
+    return {
+        connectDropTarget: connect.dropTarget(),
+        isOfficialOver: monitor.isOver(),
+        canDrop: monitor.canDrop()
+    };
+}
+
+
+@DropTarget('person', officialTarget, collectOfficial)
+export default class OfficialList extends React.Component {
+    static propTypes = {
+        officials: React.PropTypes.array.isRequired,
+        onSelect: React.PropTypes.func.isRequired,
+        onRemove: React.PropTypes.func.isRequired,
+        onAdd: React.PropTypes.func,
+    };
+
+    render() {
+        let addItem = this.props.connectDropTarget(
+            <li key="add" className="OfficialList-addItem">
+                <p>
+                    Drag a person here or <a
+                        onClick={ this.onClickAddOfficials.bind(this) }>
+                        select officials to be added</a>.
+                </p>
+            </li>
+        );
+
+        let classes = cx('OfficialList', {
+            'OfficialList-isPersonOver': this.props.isOfficialOver,
+        });
+
+        return (
+            <ul className={ classes }>
+                { addItem }
+            { this.props.officials.map(o => (
+                <OfficialListItem key={ o.person.id } official={ o }
+                    onSelect={ this.props.onSelect.bind(this) }
+                    onRemove={ this.props.onRemove.bind(this) }/>
+            )) }
+            </ul>
+        );
+    }
+
+    onClickAddOfficials(ev) {
+        if (this.props.onAdd) {
+            this.props.onAdd(null);
+        }
+    }
+}
--- src/components/sections/settings/OfficialsPane.jsx
+++ src/components/sections/settings/OfficialsPane.jsx
@@ -2,23 +2,78 @@ import { connect } from 'react-redux';
 import React from 'react';
 
 import PaneBase from '../../panes/PaneBase';
+import OfficialList from '../../misc/officiallist/OfficialList';
+import { createSelection } from '../../../actions/selection';
+import { retrieveOfficials } from '../../../actions/official';
 
 
 @connect(state => ({ officials: state.officials }))
 export default class OfficialsPane extends PaneBase {
+    componentDidMount() {
+        this.props.dispatch(retrieveOfficials());
+    }
+
     renderPaneContent() {
+        let officialList = this.props.officials.officialList;
+        let admins = officialList.items
+            .filter(i => i.data.role === 'admin')
+            .map(i => i.data);
+
+        let organizers = officialList.items
+            .filter(i => i.data.role === 'organizer')
+            .map(i => i.data);
+
         return [
-            <h1>Administrators</h1>,
-            <p>{
-                "Administrators have full access to all aspects of the " +
-                "organization."
-            }</p>,
-            <h1>Organizers</h1>,
-            <p>{
-                "Organizers have enough access to perform the day-to-day " +
-                "tasks related to, for example, managing a running campaign " +
-                "or an active call assignment."
-            }</p>
+            <div className="OfficialsPane-admins" key="admins">
+                <h1>Administrators</h1>
+                <p>{
+                    "Administrators have full access to all aspects of the " +
+                    "organization."
+                }</p>
+                <OfficialList officials={ admins }
+                    onAdd={ this.onAdd.bind(this, 'admin') }
+                    onRemove={ this.onRemove.bind(this) }
+                    onSelect={ this.onSelect.bind(this, 'admin') }/>
+            </div>,
+
+            <div className="OfficialsPane-organizers" key="organizers">
+                <h1>Organizers</h1>
+                <p>{
+                    "Organizers have enough access to perform the day-to-day " +
+                    "tasks related to, for example, managing a running campaign " +
+                    "or an active call assignment."
+                }</p>
+                <OfficialList officials={ organizers }
+                    onAdd={ this.onAdd.bind(this, 'organizer') }
+                    onRemove={ this.onRemove.bind(this) }
+                    onSelect={ this.onSelect.bind(this, 'organizer') }/>
+            </div>
         ];
+    }
+
+    onAdd(role, official) {
+        if (official) {
+            // TODO Add
+            console.log('Add official', official);
+        }
+        else {
+            let instructions = 'Select people to be added as officials';
+
+            let action = createSelection('person', null, instructions, ids => {
+                console.log('SELECTED', ids);
+            });
+
+            this.props.dispatch(action);
+            this.openPane('selectpeople', action.payload.id);
+        }
+    }
+
+
+    onRemove(official) {
+        console.log('REMOVE', official);
+    }
+
+    onSelect(role, official) {
+        console.log('SELECT', role, official);
     }
 }
