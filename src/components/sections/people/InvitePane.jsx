507c2697763a8f13395a349db00b2b7846ef1291
Richard Olsson <r@richardolsson.se>
2016-09-22 10:16:27 +0200

    Add InviteBox to InvitePane

--- src/components/sections/people/InvitePane.jsx
+++ src/components/sections/people/InvitePane.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import InviteBox from '../../misc/InviteBox';
 import LoadingIndicator from '../../misc/LoadingIndicator';
 import PaneBase from '../../panes/PaneBase';
 import { retrieveInvites } from '../../../actions/invite';
@@ -15,28 +16,37 @@ export default class InvitePane extends PaneBase {
     renderPaneContent(data) {
         let inviteList = this.props.invites.inviteList;
 
+        let content = [
+            <InviteBox/>
+        ];
+
         if (inviteList.isPending) {
-            return <LoadingIndicator/>;
+            content.push(<LoadingIndicator/>);
         }
         else {
             let invites = inviteList.items.map(i => i.data);
 
-            return (
-                <ul className="InvitePane-invites">
-                { invites.map(i => {
-                    return (
-                        <li key={ i.id } className="InvitePane-invite">
-                            <span className="InvitePane-time">
-                                { i.invite_time }</span>
-                            <span className="InvitePane-email">
-                                { i.email }</span>
-                            <span className="InvitePane-official">
-                                { i.official.name }</span>
-                        </li>
-                    );
-                }) }
-                </ul>
+            content.push(
+                <div className="InvitePane-invites">
+                    <h2>Sent invites</h2>
+                    <ul className="InvitePane-inviteList">
+                    { invites.map(i => {
+                        return (
+                            <li key={ i.id } className="InvitePane-invite">
+                                <span className="InvitePane-time">
+                                    { i.invite_time }</span>
+                                <span className="InvitePane-email">
+                                    { i.invite_email }</span>
+                                <span className="InvitePane-official">
+                                    { i.official.name }</span>
+                            </li>
+                        );
+                    }) }
+                    </ul>
+                </div>
             );
         }
+
+        return content;
     }
 }
