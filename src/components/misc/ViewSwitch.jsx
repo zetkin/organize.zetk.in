ea46e02087e120ff5eb3eaf0fbea1cb1137e7014
Richard Olsson <r@richardolsson.se>
2016-10-10 16:45:21 +0200

    Internationalize ViewSwitch component

--- src/components/misc/ViewSwitch.jsx
+++ src/components/misc/ViewSwitch.jsx
@@ -1,16 +1,26 @@
+import { injectIntl } from 'react-intl';
 import React from 'react';
 import cx from 'classnames';
 
 
+@injectIntl
 export default class ViewSwitch extends React.Component {
+    static propTypes = {
+        states: React.PropTypes.object.isRequired,
+        selected: React.PropTypes.string.isRequired,
+        onSwitch: React.PropTypes.func
+    };
+
     render() {
         const states = this.props.states;
         const stateNames = Object.keys(this.props.states);
 
+        const formatMessage = this.props.intl.formatMessage;
+
         return (
             <div className="ViewSwitch">
             {stateNames.map(function(state) {
-                const label = states[state];
+                const label = formatMessage({ id: states[state] });
                 const classes = cx('ViewSwitch-state-' + state, {
                     'selected': (state == this.props.selected)
                 });
@@ -29,9 +39,3 @@ export default class ViewSwitch extends React.Component {
         }
     }
 }
-
-ViewSwitch.propTypes = {
-    states: React.PropTypes.object.isRequired,
-    selected: React.PropTypes.string.isRequired,
-    onSwitch: React.PropTypes.func
-};
