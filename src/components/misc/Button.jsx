ad3f3d4f398acf83a58e6b637dfd6c0087e83677
Richard Olsson <r@richardolsson.se>
2016-10-10 16:45:09 +0200

    Internationalize Button component

--- src/components/misc/Button.jsx
+++ src/components/misc/Button.jsx
@@ -1,19 +1,24 @@
+import { injectIntl } from 'react-intl';
 import React from 'react';
 
 
+@injectIntl
 export default class Button extends React.Component {
     static propTypes = {
-        label: React.PropTypes.string.isRequired,
+        labelMsg: React.PropTypes.string.isRequired,
         onClick: React.PropTypes.func,
         className: React.PropTypes.string,
     };
 
     render() {
+        const formatMessage = this.props.intl.formatMessage;
+
+        let label = formatMessage({ id: this.props.labelMsg });
         let className = "Button " + this.props.className;
         return (
             <button className={ className }
                 onClick={ this.props.onClick }>
-                { this.props.label }
+                { label }
             </button>
         );
     }
