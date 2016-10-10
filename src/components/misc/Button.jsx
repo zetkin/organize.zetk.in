e3682f60cbb9410796ca6624e76980d5f8c5b7e5
Richard Olsson <r@richardolsson.se>
2016-10-10 20:28:26 +0200

    Add values support to button component

--- src/components/misc/Button.jsx
+++ src/components/misc/Button.jsx
@@ -6,6 +6,7 @@ import React from 'react';
 export default class Button extends React.Component {
     static propTypes = {
         labelMsg: React.PropTypes.string.isRequired,
+        labelValues: React.PropTypes.object,
         onClick: React.PropTypes.func,
         className: React.PropTypes.string,
     };
@@ -13,8 +14,10 @@ export default class Button extends React.Component {
     render() {
         const formatMessage = this.props.intl.formatMessage;
 
-        let label = formatMessage({ id: this.props.labelMsg });
         let className = "Button " + this.props.className;
+        let label = formatMessage({ id: this.props.labelMsg },
+            this.props.labelValues);
+
         return (
             <button className={ className }
                 onClick={ this.props.onClick }>
