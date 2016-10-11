2173fb76a4e06d663342986dcc22ddc1276523fd
Richard Olsson <r@richardolsson.se>
2016-10-11 09:53:44 +0200

    IntInput initialValue should be number, not string

--- src/components/forms/inputs/IntInput.jsx
+++ src/components/forms/inputs/IntInput.jsx
@@ -4,6 +4,10 @@ import InputBase from './InputBase';
 
 
 export default class IntInput extends InputBase {
+    static propTypes = {
+        initialValue: React.PropTypes.number,
+    };
+
     renderInput() {
         return (
             <input type="number" step="1" min="0" value={ this.props.value }
