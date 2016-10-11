8937dec5d6c8496680e40dd7ebf3bf9c9b0784da
Richard Olsson <r@richardolsson.se>
2016-10-11 11:46:30 +0200

    Internationalize CallPane

--- src/components/panes/CallPane.jsx
+++ src/components/panes/CallPane.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { injectIntl, FormattedMessage as Msg } from 'react-intl';
 import { connect } from 'react-redux';
 
 import PaneBase from './PaneBase';
@@ -7,6 +8,7 @@ import { retrieveCall } from '../../actions/call';
 
 
 @connect(state => ({ calls: state.calls }))
+@injectIntl
 export default class CallPane extends PaneBase {
     componentDidMount() {
         let callId = this.getParam(0);
@@ -23,26 +25,35 @@ export default class CallPane extends PaneBase {
     }
 
     getPaneTitle(data) {
+        const formatMessage = this.props.intl.formatMessage;
         if (data.callItem && !data.callItem.isPending) {
-            return 'Call to ' + data.callItem.data.target.name;
+            return formatMessage(
+                { id: 'panes.call.title' },
+                { targetName: data.callItem.data.target.name });
         }
         else {
-            return 'Call';
+            return formatMessage({ id: 'panes.call.pendingTitle' });
         }
     }
 
     getPaneSubTitle(data) {
+        const formatMessage = this.props.intl.formatMessage;
+
         if (data.callItem && !data.callItem.isPending) {
             let call = data.callItem.data;
             let date = Date.utc.create(call.allocation_time);
-            return 'Made by ' + call.caller.name + ' on ' + date.long();
+            return formatMessage({ id: 'panes.call.subTitle' }, {
+                callerName: call.caller.name,
+                date: date.long(),
+            });
         }
     }
 
     renderPaneContent(data) {
         if (data.callItem) {
             return [
-                <h3 key="notesHeader">Caller notes</h3>,
+                <Msg tagName="h3" key="notesHeader"
+                    id="panes.call.notesHeader"/>,
                 <p key="notes">
                     { data.callItem.data.notes }
                 </p>
