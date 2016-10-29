2c43908baaf630d3dfae87b741140fc9587a1281
Richard Olsson <r@richardolsson.se>
2016-10-29 16:19:42 +0200

    Allow label messages in SelectInput

--- src/components/forms/inputs/SelectInput.jsx
+++ src/components/forms/inputs/SelectInput.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import { injectIntl } from 'react-intl';
 
 import InputBase from './InputBase';
 
 
+@injectIntl
 export default class SelectInput extends InputBase {
+    static propTypes = {
+        options: React.PropTypes.object.isRequired,
+        optionLabelsAreMessages: React.PropTypes.bool,
+    };
+
     renderInput() {
         return (
             <select value={ this.props.value }
                 onChange={ this.onChange.bind(this) }>
 
-                {Object.keys(this.props.options).map(function(key) {
+                {Object.keys(this.props.options).map(key => {
                     var label = this.props.options[key];
+                    if (this.props.optionLabelsAreMessages) {
+                        label = this.props.intl.formatMessage({
+                            id: label,
+                        });
+                    }
+
                     return <option key={ key } value={ key }>
                         { label }</option>;
                 }, this)}
