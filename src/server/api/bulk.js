cbb1860a4cb14fed8df59109d3d8ff2c18ab5268
Richard Olsson <r@richardolsson.se>
2016-11-01 17:36:55 +0100

    Create bulk op API with support for person.tag op

---
+++ src/server/api/bulk.js
@@ -0,0 +1,45 @@
+import express from 'express';
+
+const bulkApi = express();
+
+bulkApi.post('/', (req, res, next) => {
+    let op = req.body.op;
+
+    if (op in operations) {
+        let exec = operations[op];
+
+        exec(req, res)
+            .then(result => {
+                res.status(200).json({
+                    ok: true,
+                });
+            })
+            .catch(err => {
+                res.status(500).json({
+                    ok: false,
+                    error: err,
+                });
+            });
+    }
+    else {
+        res.status(400).json({
+            ok: false,
+            error: 'Unknown op ' + op,
+        });
+    }
+});
+
+
+let operations = {
+    'person.tag': (req, res) => {
+        let z = req.z;
+        let orgId = req.body.orgId;
+        let promises = req.body.objects.map(id =>
+            Promise.all(req.body.config.tags.map(tag =>
+                z.resource('orgs', orgId, 'people', id, 'tags', tag).put())));
+
+        return Promise.all(promises);
+    },
+};
+
+export default bulkApi;
--- src/server/api/index.js
+++ src/server/api/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import importApi from './import';
+import bulkApi from './bulk';
 
 
 const api = express();
 
 api.use(bodyParser.json());
 api.use('/import', importApi);
+api.use('/bulk', bulkApi);
 
 
 export default api;
