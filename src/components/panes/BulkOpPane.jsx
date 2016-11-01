d22d0d48c9d45fee07920886c09f34f1c3bcdf8d
Richard Olsson <r@richardolsson.se>
2016-11-01 16:34:59 +0100

    Set title of bulk op pane

--- src/components/panes/BulkOpPane.jsx
+++ src/components/panes/BulkOpPane.jsx
@@ -14,12 +14,22 @@ const mapStateToProps = (state, props) => ({
 @injectIntl
 export default class BulkOpPane extends PaneBase {
     getRenderData() {
+        let selectionList = this.props.selections.selectionList;
+        let selectionId = this.getParam(1);
+        let selectionItem = getListItemById(selectionList, selectionId);
+
         return {
+            selection: selectionItem.data
         };
     }
 
     getPaneTitle(data) {
         const formatMessage = this.props.intl.formatMessage;
+
+        let id = 'panes.bulkOp.headers.' + this.getParam(0);
+        let count = data.selection.selectedIds.length;
+
+        return formatMessage({ id }, { count });
     }
 
     renderPaneContent(data) {
