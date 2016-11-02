302a45feeb2dc871569735e177a9faebb37c3219
Richard Olsson <r@richardolsson.se>
2016-11-02 16:02:04 +0100

    Rename PCActionParticipantItem to match usage name

--- src/components/misc/personcollection/items/PCActionParticipantItem.jsx
+++ src/components/misc/personcollection/items/PCActionParticipantItem.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 
-export default class PersonCollectionParticipantItem extends React.Component {
+export default class PCActionParticipantItem extends React.Component {
     render() {
         let item = this.props.item;
         let name = item.first_name + ' ' + item.last_name;
 
         return (
-            <div className="PersonCollectionParticipantItem">
-                <span className="PersonCollectionParticipantItem-name">
+            <div className="PCActionParticipantItem">
+                <span className="PCActionParticipantItem-name">
                     { name }</span>
             </div>
         );
