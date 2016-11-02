771bd0b2b1ce2da0c55b45329931cf758f734610
Richard Olsson <r@richardolsson.se>
2016-11-02 16:49:12 +0100

    Add response time to PCActionResponseItem

--- src/components/misc/personcollection/items/PCActionResponseItem.jsx
+++ src/components/misc/personcollection/items/PCActionResponseItem.jsx
@@ -5,11 +5,14 @@ export default class PCActionResponseItem extends React.Component {
     render() {
         let item = this.props.item;
         let name = item.person.name;
+        let responseTime = item.response_date;
 
         return (
             <div className="PCActionResponseItem">
                 <span className="PCActionResponseItem-name">
                     { name }</span>
+                <span className="PCActionResponseItem-time">
+                    { responseTime }</span>
             </div>
         );
     }
