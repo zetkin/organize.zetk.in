83902d275143e533306cf5055d0817d25aa66e21
Richard Olsson <r@richardolsson.se>
2017-01-09 09:47:55 +0100

    Fix links in dashboard footer

--- src/components/dashboard/Footer.jsx
+++ src/components/dashboard/Footer.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import { FormattedMessage as Msg } from 'react-intl';
+import { injectIntl, FormattedMessage as Msg } from 'react-intl';
 
 import Link from '../misc/Link';
 
 
+@injectIntl
 export default class Footer extends React.Component {
     render() {
+        const formatMessage = this.props.intl.formatMessage;
+
+        let foundationHref = formatMessage({
+            id: 'dashboard.footer.foundationLink.href' })
+
+        let helpHref = formatMessage({
+            id: 'dashboard.footer.helpLink.href' })
+
         return (
             <footer className="Footer">
                 <Msg tagName="p" id="dashboard.footer.info"/>
                 <ul>
                     <li>
-                        <Link href="http://zetkin.org" target="_blank"
-                            msgId="dashboard.footer.foundationLink"/>
+                        <Link href={ foundationHref } target="_blank"
+                            msgId="dashboard.footer.foundationLink.text"/>
                     </li>
                     <li>
-                        <Link href="http://manual.zetkin.org" target="_blank"
-                            msgId="dashboard.footer.helpLink"/>
+                        <Link href={ helpHref } target="_blank"
+                            msgId="dashboard.footer.helpLink.text"/>
                     </li>
                 </ul>
             </footer>
