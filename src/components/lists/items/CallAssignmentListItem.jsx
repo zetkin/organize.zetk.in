5ccbc6248914906c3c3b657d2813f1f750fdfa8d
Jens Börjesson <jens@dreamsinteractive.se>
2017-01-11 17:58:05 +0100

    Add indicator of callers count instead of ParticipantList

--- src/components/lists/items/CallAssignmentListItem.jsx
+++ src/components/lists/items/CallAssignmentListItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 import { connect } from 'react-redux';
+import { FormattedMessage as Msg } from 'react-intl';
 
 import LoadingIndicator from '../../misc/LoadingIndicator';
 import ParticipantList from './elements/ParticipantList'
@@ -44,7 +45,7 @@ export default class CallAssignmentListItem extends React.Component {
         let assignment = this.props.data;
         let targetStats = null;
         let goalStats = null;
-        let participantList = null;
+        let participantIndicator = null;
         const assignmentDateStart = new Date(assignment.start_date);
         const assignmentDateEnd = new Date(assignment.end_date);
         const inPast = (assignmentDateStart < (new Date()) ? true : false);
@@ -80,13 +81,18 @@ export default class CallAssignmentListItem extends React.Component {
         }
 
         if (assignment.callerList && assignment.callerList.isPending) {
-            participantList = <LoadingIndicator/>
+            participantIndicator = <LoadingIndicator/>
         }
         else if (assignment.callerList) {
             let participants = assignment.callerList.items.map(p => p.data);
-            participantList = <ParticipantList
-                maxVisible={ 4 }
-                participants={ participants }/>
+            const count = (participants.length)? participants.length : "0";
+            participantIndicator =  (
+                <span className="CallAssignmentListItem-infoCallers">
+                    <i className="fa fa-user"></i>
+                    <Msg id="lists.callAssignmentList.item.info.callers"
+                        values={{ count }}/>
+                </span>
+            );
         }
 
         return (
@@ -99,8 +105,7 @@ export default class CallAssignmentListItem extends React.Component {
                 <div className="CallAssignmentListItem-info">
                     <h3 className="CallAssignmentListItem-infoTitle">
                         { assignment.title }</h3>
-                    <span className="CallAssignmentListItem-infoCallers">
-                        { participantList }</span>
+                    { participantIndicator }
                 </div>
                 <div className="CallAssignmentListItem-stats">
                     <div className="CallAssignmentListItem-targetStats">
