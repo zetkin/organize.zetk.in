a2463e1b05e3348a1d34efcff75ff8c6da04a0b0
Richard Olsson <r@richardolsson.se>
2017-01-11 09:59:52 +0100

    Move action filters to CampaignSectionPaneBase
    Making them available to all the sub-sections of the Campaign section.

--- src/components/sections/campaign/CampaignSectionPaneBase.jsx
+++ src/components/sections/campaign/CampaignSectionPaneBase.jsx
@@ -1,11 +1,37 @@
 import moment from 'moment';
 import React from 'react';
 
+import CampaignSelect from '../../misc/CampaignSelect';
+import DateInput from '../../forms/inputs/DateInput';
 import RootPaneBase from '../RootPaneBase';
-import { createAction, updateAction } from '../../../actions/action';
+import {
+    createAction,
+    retrieveActions,
+    updateAction
+} from '../../../actions/action';
 
 
 export default class CampaignSectionPaneBase extends RootPaneBase {
+    getPaneFilters(data) {
+        return [
+            <CampaignSelect key="campaignSelect"
+                onCreate={ this.onCreateCampaign.bind(this) }
+                onEdit={ this.onEditCampaign.bind(this) }/>,
+            <div key="dateFilter" className="AllActionsPane-dateFilter">
+                <DateInput name="afterDate"
+                    value={ this.props.actions.filters.afterDate }
+                    labelMsg="panes.allActions.filters.afterDate"
+                    onValueChange={ this.onFilterAfterDateChange.bind(this) }
+                    />
+                <DateInput name="beforeDate"
+                    value={ this.props.actions.filters.beforeDate }
+                    labelMsg="panes.allActions.filters.beforeDate"
+                    onValueChange={ this.onFilterBeforeDateChange.bind(this) }
+                    />
+            </div>
+        ];
+    }
+
     onCalendarAddAction(date) {
         const selectedId = this.props.campaigns.selectedCampaign;
         const campParam = selectedId || 0;
@@ -79,5 +105,15 @@ export default class CampaignSectionPaneBase extends RootPaneBase {
 
     onCreateCampaign(title) {
         this.openPane('addcampaign', title);
+    }
+
+    onFilterAfterDateChange(name, value) {
+        let beforeDate = this.props.actions.filters.beforeDate;
+        this.props.dispatch(retrieveActions(value, beforeDate));
+    }
+
+    onFilterBeforeDateChange(name, value) {
+        let afterDate = this.props.actions.filters.afterDate;
+        this.props.dispatch(retrieveActions(afterDate, value));
     }
 }
