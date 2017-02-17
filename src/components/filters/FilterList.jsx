3c091696006764db84e8bb8b3e48e665090ba5af
Richard Olsson <r@richardolsson.se>
2017-02-17 22:18:31 +0100

    Remove placeholder JoinDateFilter
    Closes #420

--- src/components/filters/FilterList.jsx
+++ src/components/filters/FilterList.jsx
@@ -70,7 +70,6 @@ export default class FilterList extends React.Component {
             'all': msg('filters.types.all'),
             'call_history': msg('filters.types.callHistory'),
             'campaign_participation': msg('filters.types.campaignParticipation'),
-            'join_date': msg('filters.types.joinDate'),
             'person_data': msg('filters.types.personData'),
             'person_tags': msg('filters.types.personTags'),
             'random': msg('filters.types.random'),
