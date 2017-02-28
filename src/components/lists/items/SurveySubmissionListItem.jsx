ac39edc90c755fc1a5bc10e51b5fea67ca4e115a
Jens Börjesson <jens@dreamsinteractive.se>
2017-02-28 13:37:52 +0100

    Add styling for item, name and avatar in SurveySubmissionListItem

--- src/components/lists/items/SurveySubmissionListItem.jsx
+++ src/components/lists/items/SurveySubmissionListItem.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
+import Avatar from '../../misc/Avatar';
 import DraggableAvatar from '../../misc/DraggableAvatar';
-
+import { FormattedMessage as Msg } from 'react-intl';
 
 export default class SurveySubmissionListItem extends React.Component {
     static propTypes = {
@@ -11,11 +12,33 @@ export default class SurveySubmissionListItem extends React.Component {
 
     render() {
         let submission = this.props.data;
+        if (!submission) return null;
+
+        let timestamp = Date.create(submission.submitted);
 
         let respondent = '';
+        let respondentAvatar = null;
+        let respondentName = null;
+
         if (submission.respondent) {
-            respondent = submission.respondent.first_name
-                + ' ' + submission.respondent.last_name;
+            respondentName =
+                submission.respondent.first_name
+                + ' ' +
+                submission.respondent.last_name;
+
+            if (submission.respondent.id) {
+                respondentAvatar =
+                    <Avatar person={ submission.respondent }/>;
+            }
+            else {
+                respondentAvatar =
+                    <div className="SurveySubmissionListItem-unknown"/>;
+            }
+        }
+        else {
+            respondentAvatar =
+                <div className="SurveySubmissionListItem-anonymous"/>;
+            respondentName = <Msg id="lists.surveySubmissionList.item.anonymous"/>;
         }
 
         let surveyTitle = '';
@@ -26,10 +49,26 @@ export default class SurveySubmissionListItem extends React.Component {
         return (
             <div className="SurveySubmissionListItem"
                 onClick={ this.props.onItemClick }>
-                <span className="SurveySubmissionListItem-survey">
-                    { surveyTitle }</span>
-                <span className="SurveySubmissionListItem-respondent">
-                    { respondent }</span>
+                <div className="ListItem-date">
+                    <span className="date">
+                        { timestamp.format('{d}/{M}, {yyyy}') }</span>
+                    <span className="time">
+                        { timestamp.format('{HH}:{mm}') }</span>
+                </div>
+                <div className="SurveySubmissionListItem-content">
+                    <div className="SurveySubmissionListItem-survey">
+                        { surveyTitle }</div>
+                        <div className="SurveySubmissionListItem-respondent">
+                            <div className="SurveySubmissionListItem-avatar">
+                                { respondentAvatar }
+                            </div>
+                            <span className="SurveySubmissionListItem-name">
+                                { respondentName }
+                            </span>
+                        </div>
+                    <div className="SurveySubmissionListItem-progress"/>
+                </div>
+                <div className="SurveySubmissionListItem-action"/>
             </div>
         );
     }
