5ca226b37059a24abade7f5b1afc853429abe078
Richard Olsson <r@richardolsson.se>
2017-02-28 13:57:58 +0100

    Internationalize ActionDayMatch

--- src/components/header/search/matches/ActionDayMatch.jsx
+++ src/components/header/search/matches/ActionDayMatch.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FormattedDate, FormattedMessage as Msg } from 'react-intl';
 import url from 'url';
 
 import MatchBase from './MatchBase';
@@ -6,9 +7,20 @@ import MatchBase from './MatchBase';
 
 export default class ActionDayMatch extends MatchBase {
     getTitle() {
-        const count = this.props.data.action_count;
-        const date = this.props.data.date;
+        let count = this.props.data.action_count;
+        let dateValue = Date.create(this.props.data.date);
 
-        return count + ' actions on ' + date;
+        let date = (
+            <FormattedDate value={ dateValue }
+                day="numeric"
+                month="long"
+                weekday="long"
+                />
+        );
+
+        return (
+            <Msg id="header.search.matches.actionDay"
+                values={{ count, date }}/>
+        );
     }
 }
