d86560a6958e2817d9ee6e529cf1f14b88036062
Jens Börjesson <jens@dreamsinteractive.se>
2017-02-28 15:00:10 +0100

    Add header columns in SurveySubmissionList

--- src/components/lists/SurveySubmissionList.jsx
+++ src/components/lists/SurveySubmissionList.jsx
@@ -16,8 +16,26 @@ export default class SurveySubmissionList extends React.Component {
     }
 
     render() {
+        let columns = [
+            {
+                'submitted':
+                    'lists.surveySubmissionList.header.time',
+            },
+            {
+                'first_name':
+                    'lists.surveySubmissionList.header.respondent',
+                'survey.title':
+                    'lists.surveySubmissionList.header.survey',
+            },
+            {
+                'is_connected':
+                    'lists.surveySubmissionList.header.connected'
+            }
+        ];
+
         return (
             <List className="SurveySubmissionList"
+                headerColumns={ columns }
                 itemComponent={ SurveySubmissionListItem }
                 list={ this.props.submissionList }
                 onItemClick={ this.props.onItemClick }
