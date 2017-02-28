190102989b79b338a87f550e0562497ea53980c8
Richard Olsson <r@richardolsson.se>
2017-02-28 14:00:19 +0100

    Internationalize QueryMatch

--- src/components/header/search/matches/QueryMatch.jsx
+++ src/components/header/search/matches/QueryMatch.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import url from 'url';
+import { FormattedMessage as Msg } from 'react-intl';
 
 import MatchBase from './MatchBase';
 
 
 export default class QueryMatch extends MatchBase {
     getTitle() {
-        return 'Person query: ' + this.props.data.title;
+        let values = {
+            title: this.props.data.title,
+        };
+
+        return (
+            <Msg id="header.search.matches.query"
+                values={ values }/>
+        );
     }
 }
