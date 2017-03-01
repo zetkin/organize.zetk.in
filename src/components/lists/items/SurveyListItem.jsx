c7c8da936484f6c4ea3d39f14f977f96fd09dacd
Jens Börjesson <jens@dreamsinteractive.se>
2017-03-01 17:15:36 +0100

    Add structure to SurveyListItem and add access and callers_only

--- src/components/lists/items/SurveyListItem.jsx
+++ src/components/lists/items/SurveyListItem.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import cx from 'classnames';
 
 import DraggableAvatar from '../../misc/DraggableAvatar';
+import { FormattedMessage as Msg } from 'react-intl';
 
 
 export default class SurveyListItem extends React.Component {
@@ -11,12 +13,37 @@ export default class SurveyListItem extends React.Component {
 
     render() {
         let survey = this.props.data;
+        let access = null;
+        let callers_only = null;
+
+        if (survey.access === "open") {
+            access = <Msg id="lists.surveyList.item.access.open"/>;
+        }
+        else if (survey.access === "auth") {
+            access = <Msg id="lists.surveyList.item.access.auth"/>;
+        }
+
+        let accessClassNames  = cx('SurveyListItem-access', survey.access );
+
+        if (survey.callers_only) {
+            callers_only = (
+                <span className="SurveyListItem-callers">
+                    <Msg id="lists.surveyList.item.callersOnly"/>
+                </span>
+            );
+        }
 
         return (
             <div className="SurveyListItem"
                 onClick={ this.props.onItemClick }>
-                <span className="SurveyListItem-title">
-                    { survey.title }</span>
+                <div className="SurveyListItem-content">
+                    <span className="SurveyListItem-title">
+                        { survey.title }</span>
+                    <span className={ accessClassNames }>
+                        { access }
+                    </span>
+                    { callers_only }
+                </div>
             </div>
         );
     }
