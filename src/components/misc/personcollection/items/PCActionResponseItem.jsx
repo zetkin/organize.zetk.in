373e896aeb98bbf5c2fc53a0f84f960c70106ba0
Jens Börjesson <jens@dreamsinteractive.se>
2017-04-26 17:10:35 +0200

    Format date in PCActionResponseItem. Tweak droparea styling.

--- src/components/misc/personcollection/items/PCActionResponseItem.jsx
+++ src/components/misc/personcollection/items/PCActionResponseItem.jsx
@@ -5,14 +5,16 @@ export default class PCActionResponseItem extends React.Component {
     render() {
         let item = this.props.item;
         let name = item.person.name;
-        let responseTime = item.response_date;
+        let responseTime = Date.create(item.response_date);
+        let responseTimeLabel = responseTime.setUTC(true)
+                .format('{yyyy}-{MM}-{dd} {HH}:{mm}');
 
         return (
             <div className="PCActionResponseItem">
                 <span className="PCActionResponseItem-name">
                     { name }</span>
                 <span className="PCActionResponseItem-time">
-                    { responseTime }</span>
+                    { responseTimeLabel }</span>
             </div>
         );
     }
