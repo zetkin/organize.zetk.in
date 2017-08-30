5dd90430a7b38be4e67c1329fe7f9091040858b1
Richard Olsson <r@richardolsson.se>
2017-08-30 17:15:11 +0200

    Implement weighted selection when generating routes

--- src/actions/route.js
+++ src/actions/route.js
@@ -13,8 +13,6 @@ export function generateRoutes(addressIds, config = {}) {
         // TODO: Implement auto-planner algorithm
         let points = getNormalizedPoints(addresses);
 
-        populateNeighbours(points);
-
         while (points.length) {
             routes.push(findRoute(points, routeSize));
         }
@@ -42,51 +40,42 @@ export function discardRouteDrafts() {
 
 function findRoute(points, size) {
     let cur = points[Math.floor(Math.random() * points.length)];
-    let out = [];
+    let cluster = new Cluster();
     let queue = [];
 
-    while (cur && out.length < size) {
+    while (cur && cluster.numPoints() < size) {
         let idx = points.indexOf(cur);
-        let neighbours = cur.getNeighbours();
 
         points.splice(idx, 1);
         cur.removeAllNeighbours();
-        out.push(cur);
 
-        queue = queue.concat(neighbours);
+        cluster.addPoint(cur);
+
+        //queue = queue.concat(neighbours);
+        queue = points.map(o => ({
+            point: o,
+            weight: cluster.fitForCluster(o)
+        }));
+
         cur = null;
+
+        // Sort queue based on weights
+        queue.sort((a, b) => (a.weight - b.weight));
 
         while (!cur && queue.length) {
             let next = queue.pop();
 
-            if (!out.find(p => p.id == next.id)) {
-                cur = next;
+            if (cluster.hasPointWithId(next.point.id)) {
+                continue;
+            }
+
+            if (cluster.distanceFromNearest(next.point) < 0.2) {
+                cur = next.point;
             }
         }
     }
 
-    return {
-        id: Math.random().toString(),
-        addresses: out.map(p => p.id),
-    };
-}
-
-function populateNeighbours(points, count=5) {
-    points.forEach(p => {
-        let others = points.map(o => ({
-            dist: ((p.x - o.x)*(p.x - o.x) + (p.y - o.y)*(p.y - o.y)),
-            point: o,
-        }));
-
-        others.sort((a,b) => a.dist - b.dist);
-
-        // TODO: Filter those that are TOO far away
-        others.slice(0, count).forEach(o => {
-            if (!p.isNeighbour(o.point)) {
-                new Connection(p, o.point);
-            }
-        });
-    });
+    return cluster.toRouteDraft();
 }
 
 function getNormalizedPoints(addresses) {
@@ -99,6 +88,7 @@ function getNormalizedPoints(addresses) {
         addr.id,
         (addr.longitude - b.minX) / maxDim,
         (addr.latitude - b.minY) / maxDim,
+        addr.street,
     ));
 }
 
@@ -131,10 +121,11 @@ function calcBounds(addresses) {
     return b;
 }
 
-function Point(id, x, y) {
+function Point(id, x, y, street) {
     this.id = id;
     this.x = x;
     this.y = y;
+    this.street = street;
 
     let _connections = {};
 
@@ -148,6 +139,8 @@ function Point(id, x, y) {
         delete _connections[o.id];
     };
 
+    this.getWeight = o => _connections[o.id].weight;
+
     this.isNeighbour = o => !!_connections[o.id];
 
     this.removeAllNeighbours = () => {
@@ -156,23 +149,97 @@ function Point(id, x, y) {
         });
     };
 
+    this.squareDistanceTo = p =>
+        (p.x - this.x)*(p.x - this.x) + (p.y - this.y)*(p.y - this.y);
+
+    this.distanceTo = p => Math.sqrt(this.squareDistanceTo(p));
+
     this.getNeighbours = () =>
         Object.keys(_connections)
             .map(id => _connections[id].getOther(this));
 }
 
-function Connection(p0, p1) {
-    let _p0 = p0;
-    let _p1 = p1;
+function Cluster() {
+    let _points = [];
+    let _streets = {};
+    let _minX = Infinity;
+    let _minY = Infinity;
+    let _maxX = -Infinity;
+    let _maxY = -Infinity;
+    let _cX = 0;
+    let _cY = 0;
 
-    this.getOther = s => (_p0 == s)? _p1 : _p0;
+    let _totX = 0;
+    let _totY = 0;
+    let _aX = 0;
+    let _aY = 0;
 
-    this.clear = () => {
-        _p0.removeConnection(this);
-        _p1.removeConnection(this);
-        _p0 = _p1 = null;
+    this.addPoint = p => {
+        _points.push(p);
+        _minX = Math.min(p.x, _minX);
+        _minY = Math.min(p.y, _minY);
+        _maxX = Math.max(p.x, _maxX);
+        _maxY = Math.max(p.y, _maxY);
+        _cX = (_maxX - _minX) / 2;
+        _cY = (_maxY - _minY) / 2;
+
+        _totX += p.x;
+        _totY += p.y;
+        _aX = _totX / _points.length;
+        _aY = _totY / _points.length;
+
+        if (!_streets[p.street])
+            _streets[p.street] = 0;
+
+        _streets[p.street]++;
     };
 
-    p0.addConnection(this);
-    p1.addConnection(this);
+    this.numPoints = () => _points.length;
+    this.hasPointWithId = id => !!_points.find(p => p.id == id);
+
+    this.fitForCluster = p => {
+        let fitness = 0;
+
+        fitness += 100 - this.distanceFromAverage(p) * 100;
+        fitness += 50 - this.distanceFromCenter(p) * 50;
+        fitness += _streets[p.street] || 0;
+
+        if (_points.length > 2) {
+            let dn = _points.length / ((_maxX - _minX) * (_maxY - _minY)) || 1;
+            let maxX = Math.max(p.x, _maxX);
+            let maxY = Math.max(p.y, _maxY);
+            let minX = Math.min(p.x, _minX);
+            let minY = Math.min(p.y, _minY);
+            let da = (_points.length + 1) / ((maxX - minX) * (maxY - minY)) || 1;
+
+            fitness += (da - dn) * 10;
+        }
+
+        return fitness;
+    };
+
+    this.squareDistanceFromCenter = p =>
+        (p.x - _cX)*(p.x - _cX) + (p.y - _cY)*(p.y - _cY);
+
+    this.distanceFromCenter = p => Math.sqrt(this.squareDistanceFromCenter(p));
+
+    this.squareDistanceFromAverage = p =>
+        (p.x - _aX)*(p.x - _aX) + (p.y - _aY)*(p.y - _aY);
+
+    this.distanceFromAverage = p => Math.sqrt(this.squareDistanceFromAverage(p));
+
+    this.distanceFromNearest = p => {
+        let min = Infinity;
+
+        _points.forEach(o => {
+            min = Math.min(min, o.squareDistanceTo(p));
+        });
+
+        return Math.sqrt(min);
+    };
+
+    this.toRouteDraft = () => ({
+        id: Math.random().toString(),
+        addresses: _points.map(p => p.id),
+    });
 }
