db01bbb8965ddbc6ff7a46e85d16be24d7ec0994
Richard Olsson <r@richardolsson.se>
2017-09-22 11:23:29 +0200

    Open RoutePane when clicking finished route in RoutePanel

--- src/components/sections/canvass/elements/RoutePanel.jsx
+++ src/components/sections/canvass/elements/RoutePanel.jsx
@@ -66,6 +66,7 @@ export default class RoutePanel extends React.Component {
         else {
             return (
                 <RouteList list={ routeList }
+                    onRouteClick={ this.props.onRouteClick }
                     onRouteMouseOver={ this.onRouteMouseOver.bind(this) }
                     onRouteMouseOut={ this.onRouteMouseOut.bind(this) }
                     />
@@ -121,6 +122,7 @@ export default class RoutePanel extends React.Component {
                             </p>
                         </div>
                         <RouteList list={ draftList }
+                            onRouteClick={ this.props.onRouteClick }
                             onRouteMouseOver={ this.onRouteMouseOver.bind(this) }
                             onRouteMouseOut={ this.onRouteMouseOut.bind(this) }
                             />
