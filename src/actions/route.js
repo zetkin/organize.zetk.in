afac9cb16edc6a138e5892091caa2b08d30effef
Richard Olsson <r@richardolsson.se>
2017-10-02 11:26:10 +0200

    Add a mock createRoute() action, used in RouteFromAddressesPane

--- src/actions/route.js
+++ src/actions/route.js
@@ -1,4 +1,5 @@
 import * as types from '.';
+import makeRandomString from '../utils/makeRandomString';
 
 const ROUTE_GEN_DEFAULTS = {
     routeSize: 100,
@@ -11,6 +12,35 @@ if (typeof window !== 'undefined') {
     let worker = new RouteGenWorker();
 }
 
+
+export function createRoute(addressIds, paneId) {
+    return ({ dispatch, getState }) => {
+        // TODO: Remove once hooked to API
+        let addressById = getState().addresses.addressById;
+        let households = addressIds
+            .map(id => addressById[id])
+            .reduce((sum, addr) => sum + addr.household_count, 0);
+
+        dispatch({
+            type: types.CREATE_ROUTE,
+            meta: { paneId },
+            payload: {
+                // TODO: Submit to API
+                promise: new Promise((resolve) => {
+                    resolve({
+                        data: {
+                            data: {
+                                id: makeRandomString(6),
+                                addresses: addressIds,
+                                household_count: households,
+                            }
+                        }
+                    });
+                }),
+            }
+        });
+    };
+}
 
 export function generateRoutes(addressIds, config = {}) {
     config = Object.assign({}, ROUTE_GEN_DEFAULTS, config);
