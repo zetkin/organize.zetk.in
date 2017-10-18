95db0e23b9cb8a9a5d4c733cd8b78cddc5f00206
Richard Olsson <r@richardolsson.se>
2017-10-18 15:51:17 +0200

    Create addressVisitList from householdVisitList in store

--- src/store/visits.js
+++ src/store/visits.js
@@ -7,15 +7,41 @@ import {
 
 export default function visits(state = null, action) {
     if (action.type == types.RETRIEVE_HOUSEHOLD_VISITS + '_FULFILLED') {
-        let visits = action.payload.data.data;
+        let householdVisits = action.payload.data.data;
+        let visitsByAddress = {};
+
+        householdVisits.forEach(hhv => {
+            let addr = hhv.address;
+            if (!visitsByAddress.hasOwnProperty(addr.id)) {
+                visitsByAddress[addr.id] = {
+                    id: addr.id,
+                    address: addr.address,
+                    state: hhv.state,
+                    households_visited: 0,
+                    households_allocated: 0,
+                };
+            }
+
+            visitsByAddress[addr.id].households_allocated++;
+
+            if (hhv.visit_time) {
+                visitsByAddress[addr.id].households_visited++;
+            }
+        });
+
+        let addressVisits = Object.keys(visitsByAddress).map(id => visitsByAddress[id]);
+
         return Object.assign({}, state, {
+            addressVisitList: updateOrAddListItems(
+                state.addressVisitList, addressVisits),
             householdVisitList: updateOrAddListItems(
-                state.householdVisitList, visits),
+                state.householdVisitList, householdVisits),
         });
     }
     else {
         return state || {
-            householdVisitList: createList,
+            addressVisitList: createList(),
+            householdVisitList: createList(),
         };
     }
 }
