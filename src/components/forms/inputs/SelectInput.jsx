0d5a2912b2231402a137a1cb3d946d4b49e58307
Richard Olsson <r@richardolsson.se>
2017-10-30 10:16:01 +0100

    Add orderAlphabetically property to SelectInput

--- src/components/forms/inputs/SelectInput.jsx
+++ src/components/forms/inputs/SelectInput.jsx
@@ -9,14 +9,21 @@ export default class SelectInput extends InputBase {
     static propTypes = {
         options: React.PropTypes.object.isRequired,
         optionLabelsAreMessages: React.PropTypes.bool,
+        orderAlphabetically: React.PropTypes.bool,
     };
 
     renderInput() {
+        let keys = Object.keys(this.props.options);
+
+        if (this.props.orderAlphabetically) {
+            keys.sort((k0, k1) => this.props.options[k0].localeCompare(this.props.options[k1]));
+        }
+
         return (
             <select value={ this.props.value }
                 onChange={ this.onChange.bind(this) }>
 
-                {Object.keys(this.props.options).map(key => {
+                {keys.map(key => {
                     var label = this.props.options[key];
                     if (this.props.optionLabelsAreMessages) {
                         label = this.props.intl.formatMessage({
