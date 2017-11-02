318bd62a8a7417d144c256d02be6c4325de0ba2a
Richard Olsson <r@richardolsson.se>
2017-11-02 13:55:49 +0100

    Implement sorting of AssignedRouteList

--- src/components/lists/AssignedRouteList.jsx
+++ src/components/lists/AssignedRouteList.jsx
@@ -28,9 +28,49 @@ export default class AssignedRouteList extends React.Component {
         return (
             <List className="AssignedRouteList"
                 headerColumns={ columns }
+                sortFunc={ sortFunc }
                 itemComponent={ AssignedRouteListItem }
                 list={ this.props.assignedRouteList }
                 onItemClick={ this.props.onItemClick }/>
         );
     }
 }
+
+let sortFunc = (i0, i1, field) => {
+    let ar0 = i0.data;
+    let ar1 = i1.data;
+
+    if (field == 'assignment') {
+        return ar0.assignment.title.localeCompare(ar1.assignment.title);
+    }
+    else if (field == 'route') {
+        let r0 = ar0.route;
+        let r1 = ar1.route;
+
+        if (r0.title && !r1.title) {
+            return -1;
+        }
+        else if (r1.title && !r0.title) {
+            return 1;
+        }
+        else {
+            let t0 = r0.title || r0.id;
+            let t1 = r1.title || r1.id;
+            return t0.localeCompare(t1);
+        }
+    }
+    else if (field == 'canvasser') {
+        if (ar0.canvasser && !ar1.canvasser) {
+            return -1;
+        }
+        else if (ar1.canvasser && !ar0.canvasser) {
+            return 1;
+        }
+        else {
+            return ar0.canvasser.name.localeCompare(ar1.canvasser.name);
+        }
+    }
+    else {
+        return 0;
+    }
+};
