afad9953142ce2b4a0876fb1ef6880ac801b9145
Richard Olsson <r@richardolsson.se>
2018-04-24 19:35:50 +0200

    Create barebones GroupList component

---
+++ src/components/lists/items/GroupListItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import DraggableAvatar from '../../misc/DraggableAvatar';
+
+
+export default class GroupListItem extends React.Component {
+    static propTypes = {
+        onItemClick: React.PropTypes.func,
+        data: React.PropTypes.shape({
+            id: React.PropTypes.number.isRequired,
+            title: React.PropTypes.string.isRequired,
+        })
+    }
+
+    render() {
+        let group = this.props.data;
+
+        return (
+            <div className="GroupListItem"
+                onClick={ this.props.onItemClick }>
+
+                <div className="GroupListItem-col">
+                    <span className="GroupListItem-title">
+                        { group.title }</span>
+                </div>
+            </div>
+        );
+    }
+}
+
