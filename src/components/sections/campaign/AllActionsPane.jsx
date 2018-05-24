5b65f103a586d3af2f360abf4c7d446c3fde9fb1
Victor Baquero Wihlborg <jag@vbw.se>
2018-05-24 13:18:28 +0200

    Filter passed actions in list

--- src/components/sections/campaign/AllActionsPane.jsx
+++ src/components/sections/campaign/AllActionsPane.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { FormattedMessage as Msg } from 'react-intl';
 
 import Button from '../../misc/Button';
 
@@ -27,31 +28,32 @@ export default class AllActionsPane extends CampaignSectionPaneBase {
 
         this.state = Object.assign({}, this.state, {
             viewMode: 'cal',
+            showOldActions: false
         });
     }
-
+    
     componentDidMount() {
         super.componentDidMount();
-
+        
         if (!this.props.filteredActionList) {
             this.props.dispatch(retrieveActions());
             this.props.dispatch(retrieveActivities());
             this.props.dispatch(retrieveCampaigns());
         }
     }
-
+    
     renderPaneContent() {
         let actionList = this.props.filteredActionList;
         if (!actionList || !actionList.items) {
             return null;
         }
-
+        
         let viewComponent;
-
+        
         // Use afterDate filter as startDate, or today if it's null
         let startDate = this.props.actions.filters.afterDate?
-            Date.create(this.props.actions.filters.afterDate) : new Date();
-
+        Date.create(this.props.actions.filters.afterDate) : new Date();
+        
         // Default to no end date (which displays all actions) or, if there
         // are no actions, use 8 weeks in future. If there is a filter, use
         // that date.
@@ -62,21 +64,60 @@ export default class AllActionsPane extends CampaignSectionPaneBase {
         else if (actionList.items.length == 0) {
             endDate = startDate.clone().addDays(8 * 7);
         }
-
+        
         if (this.state.viewMode == 'cal') {
             let actions = actionList.items.map(i => i.data);
-
+            
             viewComponent = <ActionCalendar actions={ actions }
-                    startDate={ startDate } endDate={ endDate }
-                    onSelectDay={ this.onSelectDay.bind(this) }
-                    onAddAction={ this.onCalendarAddAction.bind(this) }
-                    onMoveAction={ this.onCalendarMoveAction.bind(this) }
-                    onCopyAction={ this.onCalendarCopyAction.bind(this) }
-                    onSelectAction={ this.onSelectAction.bind(this) }/>
+            startDate={ startDate } endDate={ endDate }
+            onSelectDay={ this.onSelectDay.bind(this) }
+            onAddAction={ this.onCalendarAddAction.bind(this) }
+            onMoveAction={ this.onCalendarMoveAction.bind(this) }
+            onCopyAction={ this.onCalendarCopyAction.bind(this) }
+            onSelectAction={ this.onSelectAction.bind(this) }/>
         }
         else {
-            viewComponent = <ActionList actionList={ actionList }
+            const {showOldActions} = this.state;
+            const now = new Date();
+            let allActions = [];
+            let futureActions = [];
+            let actions;
+            let oldActionNotice;
+    
+            actionList.items.forEach( action => {
+                const actionEnd = new Date(action.data.end_time);
+                allActions.push(action)
+                if (now < actionEnd) {
+                    futureActions.push(action)
+                }
+            });
+
+            if (!showOldActions && futureActions.length < allActions.length) {
+                actions = [...futureActions];
+                oldActionNotice = <div key="oldActions" className="AllActionsPane-oldActions">
+                    <Msg id="lists.actionList.oldActions.notice"
+                    values={{ count: allActions.length - futureActions.length }}/>
+                    <div className="AllActionsPane-oldActionsButtons" >
+                        <span className="AllActionsPane-oldActionsButton" onClick={this.onShowOldClick.bind(this)}>
+                            <Msg id="lists.actionList.oldActions.showOld"/>
+                        </span>
+                        <span className="AllActionsPane-oldActionsButton" onClick={this.onFilterButtonClick.bind(this)}>
+                            <Msg id="lists.actionList.oldActions.changeFilter" />
+                        </span>
+                    </div>
+                    
+                </div>;
+            }
+            else {
+                actions = [...allActions]
+            }
+
+            viewComponent = [
+                oldActionNotice,
+                <ActionList actionList={ {items: actions} }
+                key="actionList"
                 onItemClick={ (item, ev) => this.onSelectAction(item.data, ev) }/>
+            ]
         }
 
         return viewComponent;
@@ -104,8 +145,15 @@ export default class AllActionsPane extends CampaignSectionPaneBase {
             viewMode: state
         });
     }
-
+    
     onAddClick() {
         this.openPane('addaction');
+    }
+
+    onShowOldClick() {
+        console.log('show old');
+        this.setState({
+            showOldActions: true
+        });
     }
 }
