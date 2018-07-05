561f996b4281250cdf980c41406341dddf82ffbe
Richard Olsson <r@richardolsson.se>
2018-07-05 09:05:23 +0200

    Add action and store logic to delete action responses

--- src/store/actionResponses.js
+++ src/store/actionResponses.js
@@ -9,6 +9,17 @@ export default function actionResponses(state = null, action) {
         byAction[actionId] = action.payload.data.data;
         return Object.assign({}, state, { byAction: byAction });
     }
+    else if (action.type == types.DELETE_ACTION_RESPONSE + '_FULFILLED') {
+        const actionId = action.meta.actionId;
+        const responses = (state.byAction[actionId] || []);
+
+        return Object.assign({}, state, {
+            byAction: {
+                [actionId]: responses
+                    .filter(r => r.person.id != action.meta.personId),
+            }
+        });
+    }
     else {
         return state || {
             byAction: {},
