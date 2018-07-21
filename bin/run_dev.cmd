740fd21d4c31b8e2d024646dcf073bb5fdc7e416
Olle Jonsson <olle.jonsson@gmail.com>
2018-07-21 10:13:10 +0200

    24-digit env var TOKEN_SECRET

--- bin/run_dev.cmd
+++ bin/run_dev.cmd
@@ -6,6 +6,7 @@ docker run ^
     -v %cd%/locale:/var/app/locale ^
     -v %cd%/src:/var/app/src ^
     --name organize_zetk_in ^
+    --env TOKEN_SECRET=012345678901234567891234 ^
     --env ZETKIN_LOGIN_URL=http://login.dev.zetkin.org ^
     --env ZETKIN_APP_ID=b420d6e07e3d41d096d0a964ab85e780 ^
     --env ZETKIN_APP_KEY=YWE3MzJlYWMtM2Q1Ny00NjQ4LWFjOTUtNDdjMTIxZmUxMDk5 ^
