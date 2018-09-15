bb6b1ba6e6b20066385f6962dbd455048bd2dadd
Olle Jonsson <olle.jonsson@gmail.com>
2018-09-15 12:07:39 +0200

    Drop console.log

--- src/components/forms/inputs/TimeInput.jsx
+++ src/components/forms/inputs/TimeInput.jsx
@@ -42,7 +42,6 @@ export default class TimeInput extends InputBase {
     }
 
     onHourFocus(ev) {
-        console.log('focus hour!');
         const hourDOMNode = ReactDOM.findDOMNode(this.refs.hourInput);
         hourDOMNode.select();
     }
