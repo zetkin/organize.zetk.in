026c7e50043e1182771c5dbb2f709cd7bd942673
Stefan Fisk <contact@stefanfisk.com>
2019-04-17 10:26:37 +0200

    Add available credits to SmsDistributionCreditsPane

--- src/components/sections/dialog/SmsDistributionCreditsPane.jsx
+++ src/components/sections/dialog/SmsDistributionCreditsPane.jsx
@@ -3,9 +3,16 @@ import { connect } from 'react-redux';
 import { FormattedMessage as Msg } from 'react-intl';
 
 import RootPaneBase from '../RootPaneBase';
+
+import LoadingIndicator from '../../misc/LoadingIndicator';
+
 import {
     retrieveSmsDistributionCredits,
 } from '../../../actions/smsDistribution';
+import Button from '../../misc/Button';
+
+const cn = (suffix = '') => `SmsDistributionCreditsPane${suffix}`;
+const msgId = suffix => `panes.smsDistributionCredits.${suffix}`;
 
 const mapStateToProps = state => ({
     creditsItem: state.smsDistributions.creditsItem,
@@ -24,11 +31,60 @@ export default class SmsDistributionCreditsPane extends RootPaneBase {
             creditsItem,
         } = this.props;
 
+        const isLoaded = !!(creditsItem && !creditsItem.isPending && creditsItem.data);
+
+        if (!isLoaded) {
+            return {
+                isLoaded,
+            };
+        }
+
+        const creditsData = creditsItem.data;
+
+        const availableCredits = creditsData.available;
+
         return {
+            isLoaded,
+
+            availableCredits,
         };
     }
 
     renderPaneContent(data) {
-        return 'CONTENT PLACEHOLDER';
+        const { isLoaded } = data;
+
+        if (!isLoaded) {
+            return <LoadingIndicator />
+        }
+
+        return (
+            <div className={cn()}>
+                <div className={cn('-row')}>
+                    <div className={cn('-col')}>
+                        {this.renderAvailableCredits(data)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    renderAvailableCredits({ availableCredits }) {
+        return (
+            <div className={cn('-availableCredits')}>
+                <Msg tagName="h3" id={msgId('availableCredits.title')} />
+
+                <div className={cn('-availableCreditsInner')}>
+                    <Msg id={msgId('availableCredits.credits')}
+                        values={{ credits: availableCredits }} />
+                </div>
+
+                <Button labelMsg={msgId('availableCredits.purchaseButton')}
+                    onClick={this.onPurchaseCreditsClick.bind(this)} />
+            </div>
+        )
+    }
+
+    onPurchaseCreditsClick() {
+        this.openPane('purchasesmsdistributioncredits');
     }
 }
