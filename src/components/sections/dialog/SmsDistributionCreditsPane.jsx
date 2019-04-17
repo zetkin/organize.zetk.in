98e7b22eb13cf1634947b9228befd009d2d4cd81
Stefan Fisk <contact@stefanfisk.com>
2019-04-17 10:44:04 +0200

    Add about to SmsDistributionCreditsPane

--- src/components/sections/dialog/SmsDistributionCreditsPane.jsx
+++ src/components/sections/dialog/SmsDistributionCreditsPane.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { FormattedMessage as Msg } from 'react-intl';
+import { injectIntl, FormattedMessage as Msg } from 'react-intl';
 
 import RootPaneBase from '../RootPaneBase';
 
@@ -10,6 +10,7 @@ import {
     retrieveSmsDistributionCredits,
 } from '../../../actions/smsDistribution';
 import Button from '../../misc/Button';
+import Link from '../../../components/misc/Link';
 
 const cn = (suffix = '') => `SmsDistributionCreditsPane${suffix}`;
 const msgId = suffix => `panes.smsDistributionCredits.${suffix}`;
@@ -19,6 +20,7 @@ const mapStateToProps = state => ({
 });
 
 @connect(mapStateToProps)
+@injectIntl
 export default class SmsDistributionCreditsPane extends RootPaneBase {
     componentDidMount() {
         super.componentDidMount();
@@ -29,12 +31,17 @@ export default class SmsDistributionCreditsPane extends RootPaneBase {
     getRenderData() {
         const {
             creditsItem,
+            intl: {
+                formatMessage,
+            },
         } = this.props;
 
         const isLoaded = !!(creditsItem && !creditsItem.isPending && creditsItem.data);
 
         if (!isLoaded) {
             return {
+                formatMessage,
+
                 isLoaded,
             };
         }
@@ -44,6 +51,8 @@ export default class SmsDistributionCreditsPane extends RootPaneBase {
         const availableCredits = creditsData.available;
 
         return {
+            formatMessage,
+
             isLoaded,
 
             availableCredits,
@@ -51,7 +60,9 @@ export default class SmsDistributionCreditsPane extends RootPaneBase {
     }
 
     renderPaneContent(data) {
-        const { isLoaded } = data;
+        const {
+            isLoaded,
+        } = data;
 
         if (!isLoaded) {
             return <LoadingIndicator />
@@ -63,6 +74,33 @@ export default class SmsDistributionCreditsPane extends RootPaneBase {
                     <div className={cn('-col')}>
                         {this.renderAvailableCredits(data)}
                     </div>
+                    <div className={cn('-col')}>
+                        {this.renderAbout(data)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    renderAbout({ formatMessage }) {
+        return (
+            <div className={cn('-about')}>
+                <Msg tagName="h3" id={msgId('about.title')} />
+
+                <div className={cn('-aboutInner')}>
+                    <div className={cn('-aboutContent')}>
+                        <Msg id={msgId('about.content')} values={{
+                            readMoreLink: <Link
+                                msgId={msgId('about.contentReadMoreLink')}
+                                href={formatMessage({ id: msgId('about.readMoreUrl') })}
+                                target="_blank" />
+                        }} />
+                    </div>
+
+                    <Link msgId={msgId('about.readMoreLink')}
+                        className={cn('-aboutReadMoreLink')}
+                        href={formatMessage({ id: msgId('about.readMoreUrl') })}
+                        target="_blank" />
                 </div>
             </div>
         );
