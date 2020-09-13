612cece87214d8e0096c284d3b2537358fdd86b5
Richard Olsson <r@richardolsson.se>
2020-09-13 18:26:00 +0200

    Update run_dev.cmd script to run properly in PowerShell

--- bin/run_dev.cmd
+++ bin/run_dev.cmd
@@ -1,16 +1,15 @@
-start docker-volume-watcher -v organize_zetk_in
-
 docker run ^
-    -v %cd%/static:/var/app/static ^
-    -v %cd%/bin:/var/app/bin ^
-    -v %cd%/locale:/var/app/locale ^
-    -v %cd%/src:/var/app/src ^
+    -v "%cd%/static:/var/app/static" ^
+    -v "%cd%/bin:/var/app/bin" ^
+    -v "%cd%/locale:/var/app/locale" ^
+    -v "%cd%/src:/var/app/src" ^
     --name organize_zetk_in ^
     --env TOKEN_SECRET=012345678901234567891234 ^
     --env ZETKIN_LOGIN_URL=http://login.dev.zetkin.org ^
-    --env ZETKIN_APP_ID=c690938258d842cba406a492b9bcfa24 ^
-    --env ZETKIN_APP_KEY=MmEyYjRhOGQtMWFjMS00Y2VhLTgwY2EtNDE1NDA1YmNhMTJj ^
+    --env ZETKIN_APP_ID=2dab3ce079234744aeb4b6cc38abdd00 ^
+    --env ZETKIN_APP_KEY=OGJlNTQyOTktMzBjNy00MzllLWIwNDgtM2RjYTA3MjZlZTIz ^
     --env ZETKIN_DOMAIN=dev.zetkin.org ^
+    --env ZETKIN_USE_TLS=0 ^
     -p 80:80 ^
     -p 81:81 ^
     -t ^
