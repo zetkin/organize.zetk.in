26f6e6e84d813a28f441260073efa170750f2f29
Richard Olsson <r@richardolsson.se>
2020-10-13 18:37:20 +0200

    Add survey_response column type to AddViewColumnPane

--- src/components/panes/AddViewColumnPane.jsx
+++ src/components/panes/AddViewColumnPane.jsx
@@ -4,12 +4,14 @@ import { connect } from 'react-redux';
 
 import AssignmentTemplate from '../misc/callAssignmentTemplates/AssignmentTemplate';
 import Button from '../misc/Button';
+import LoadingIndicator from '../misc/LoadingIndicator';
 import PaneBase from './PaneBase';
 import PersonViewColumnForm from '../forms/PersonViewColumnForm';
 import SelectInput from '../forms/inputs/SelectInput';
 import { createPersonViewColumn } from '../../actions/personView';
 import { retrievePersonTags } from '../../actions/personTag';
 import { retrieveQueries } from '../../actions/query';
+import { retrieveSurveys, retrieveSurvey } from '../../actions/survey';
 
 
 const DEFAULT_CONFIGS = {
@@ -21,12 +23,17 @@ const DEFAULT_CONFIGS = {
     },
     person_tag: {
         tag_id: null,
+    },
+    survey_response: {
+        survey_id: null,
+        question_id: null,
     }
 };
 
 
 @connect(state => ({
     queryList: state.queries.queryList,
+    surveyList: state.surveys.surveyList,
     tagList: state.personTags.tagList,
 }))
 @injectIntl
@@ -43,6 +50,7 @@ export default class AddPersonTagPane extends PaneBase {
     componentDidMount() {
         this.props.dispatch(retrievePersonTags());
         this.props.dispatch(retrieveQueries());
+        this.props.dispatch(retrieveSurveys());
     }
 
     getPaneTitle(data) {
@@ -67,6 +75,12 @@ export default class AddPersonTagPane extends PaneBase {
                 queryList={ this.props.queryList }
                 config={ this.state.config }
                 selected={ this.state.type == 'person_query' }
+                onChange={ column => this.setState(column) }
+                onSelect={ this.onTypeSelect.bind(this) }/>,
+            <SurveyResponseColumnTemplate key="survey_response"
+                surveyList={ this.props.surveyList }
+                config={ this.state.config }
+                selected={ this.state.type == 'survey_response' }
                 onChange={ column => this.setState(column) }
                 onSelect={ this.onTypeSelect.bind(this) }/>,
         ];
@@ -279,6 +293,108 @@ class PersonTagColumnTemplate extends React.Component {
             .find(item => item.data.id == column.config.tag_id);
 
         column.title = selectedTagItem.data.title;
+
+        this.props.onChange(column);
+    }
+}
+
+@connect((state, props) => ({
+    surveyList: state.surveys.surveyList,
+    elementsBySurvey: state.surveys.elementsBySurvey,
+}))
+class SurveyResponseColumnTemplate extends React.Component {
+    componentDidUpdate(prevProps) {
+        // When selected, pick the first tag and propagate config
+        /*
+        if (this.props.selected && !prevProps.selected) {
+            const { tagList } = this.props;
+
+            if (tagList && tagList.items && tagList.items.length) {
+                this.onConfigChange('tag_id', tagList.items[0].data.id);
+            }
+        }
+        */
+
+        const surveyId = this.props.config.survey_id;
+        if (surveyId != prevProps.config.survey_id) {
+            this.props.dispatch(retrieveSurvey(surveyId));
+        }
+    }
+
+    render() {
+        let surveyOptions = [];
+
+        if (this.props.surveyList && this.props.surveyList.items) {
+            surveyOptions = this.props.surveyList.items.reduce((options, item) => {
+                options[item.data.id] = item.data.title;
+                return options;
+            }, {});
+        }
+
+        let questionSelect = null;
+
+        const surveyId = this.props.config.survey_id;
+        if (surveyId) {
+            const elementList = this.props.elementsBySurvey[surveyId];
+            if (elementList && elementList.items) {
+                const questionOptions = elementList.items.reduce((options, item) => {
+                    // Ignore text blocks and non-free-text questions
+                    if (item.data.type == 'question' && item.data.question.response_type == 'text') {
+                        options[item.data.id] = item.data.question.question;
+                    }
+
+                    return options;
+                }, {});
+
+                if (elementList && elementList.items) {
+                    questionSelect = (
+                        <SelectInput name="question_id"
+                            labelMsg="panes.addViewColumn.config.surveyResponse.question"
+                            nullOptionMsg="panes.addViewColumn.config.surveyResponse.questionNullOption"
+                            options={ questionOptions }
+                            value={ this.props.config.question_id }
+                            onValueChange={ this.onConfigChange.bind(this) }
+                            />
+                    );
+                }
+            }
+            else {
+                questionSelect = <LoadingIndicator/>;
+            }
+        }
+
+        return (
+            <AssignmentTemplate type="survey_response"
+                messagePath="panes.addViewColumn.templates"
+                selected={ this.props.selected }
+                onSelect={ this.props.onSelect }
+                >
+                <SelectInput name="survey_id"
+                    labelMsg="panes.addViewColumn.config.surveyResponse.survey"
+                    nullOptionMsg="panes.addViewColumn.config.surveyResponse.surveyNullOption"
+                    options={ surveyOptions }
+                    value={ this.props.config.survey_id }
+                    onValueChange={ this.onConfigChange.bind(this) }
+                    />
+                { questionSelect }
+            </AssignmentTemplate>
+        );
+    }
+
+    onConfigChange(attr, val) {
+        const column = {
+            config: Object.assign({}, this.props.config, {
+                [attr]: val,
+            })
+        };
+
+        if (column.config.survey_id && column.config.question_id) {
+            const questionList = this.props.elementsBySurvey[column.config.survey_id];
+            const selectedQuestionItem = questionList.items
+                .find(item => item.data.id == column.config.question_id);
+
+            column.title = selectedQuestionItem.data.question.question;
+        }
 
         this.props.onChange(column);
     }
