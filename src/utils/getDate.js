ce84ccbbdfdc866c6574a13dda59d99f9674876d
niklas <niklas.vanhainen@gmail.com>
2020-10-26 21:01:05 +0100

    Fix bug in getDate that was introduced by a fix that validated four-digit year Swedish personal number with invalid control digits as proper date.

--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -2,14 +2,18 @@ const { getCountryTools } = require('verifiera');
 const Sugar = require('sugar-date');
 
 export default (dateStr) => {
-    let date = Sugar.Date(dateStr);
+let date = Sugar.Date(dateStr);
     // Check if date is valid
     if(date.isValid().raw) {
         return date.format('{yyyy}-{MM}-{dd}').raw;
     } else {
-        // Check if removing last four digits yields valid date
-        const strippedDatStr = dateStr.replace(/[^\d]/g,'').slice(0,-4);
-        date = Sugar.Date(strippedDatStr);
+        // Check if removing last four digits of a digit-only string yields valid date
+        let strippedDateStr = dateStr.replace(/[^\d]/g,'');
+        // Only do this on 12-digit dates, this is likely a Swedish personal number with a four digit year.
+        if(strippedDateStr.length == 12) {
+            strippedDateStr = strippedDateStr.slice(0,-4);
+        }
+        date = Sugar.Date(strippedDateStr);
         if(date.isValid().raw) {
             return date.format('{yyyy}-{MM}-{dd}').raw;
         }
