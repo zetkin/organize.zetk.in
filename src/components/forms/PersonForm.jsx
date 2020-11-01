4bbb12084a5b7321b0c42f5749f8ab45a229ef00
Richard Olsson <r@richardolsson.se>
2020-11-01 17:36:24 +0100

    Add custom fields to PersonForm in AddPersonPane and EditPersonPane

--- src/components/forms/PersonForm.jsx
+++ src/components/forms/PersonForm.jsx
@@ -1,56 +1,91 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Form from './Form';
+import DateInput from './inputs/DateInput';
 import TextInput from './inputs/TextInput';
 import SelectInput from './inputs/SelectInput';
 
 
 export default class PersonForm extends React.Component {
     render() {
-        var person = this.props.person || {};
-        var genderOptions = {
-            '_': 'forms.person.genderOptions.unknown',
-            'f': 'forms.person.genderOptions.female',
-            'm': 'forms.person.genderOptions.male',
-            'o': 'forms.person.genderOptions.other',
-        };
+        const { fieldTypes } = this.props;
+        if (fieldTypes && fieldTypes.items) {
+            var person = this.props.person || {};
+            var genderOptions = {
+                '_': 'forms.person.genderOptions.unknown',
+                'f': 'forms.person.genderOptions.female',
+                'm': 'forms.person.genderOptions.male',
+                'o': 'forms.person.genderOptions.other',
+            };
 
-        if (!person.gender) {
-            person.gender = '_';
-        }
+            if (!person.gender) {
+                person.gender = '_';
+            }
 
-        return (
-            <Form ref="form" {...this.props }>
-                <TextInput labelMsg="forms.person.firstName" name="first_name"
-                    initialValue={ person.first_name }/>
-                <TextInput labelMsg="forms.person.lastName" name="last_name"
-                    initialValue={ person.last_name }/>
-                <SelectInput labelMsg="forms.person.gender" name="gender"
+            const nativeFields = [
+                <TextInput labelMsg="forms.person.externalId" key="ext_id" name="ext_id"
+                    initialValue={ person.ext_id }/>,
+                <TextInput labelMsg="forms.person.firstName" key="first_name" name="first_name"
+                    initialValue={ person.first_name }/>,
+                <TextInput labelMsg="forms.person.lastName" key="last_name" name="last_name"
+                    initialValue={ person.last_name }/>,
+                <SelectInput labelMsg="forms.person.gender" key="gender" name="gender"
                     options={ genderOptions }
                     optionLabelsAreMessages={ true }
-                    initialValue={ person.gender }/>
-                <TextInput labelMsg="forms.person.email" name="email"
-                    initialValue={ person.email }/>
-                <TextInput labelMsg="forms.person.phone" name="phone"
-                    initialValue={ person.phone }/>
-                <TextInput labelMsg="forms.person.altPhone" name="alt_phone"
-                    initialValue={ person.alt_phone }/>
+                    initialValue={ person.gender }/>,
+                <TextInput labelMsg="forms.person.email" key="email" name="email"
+                    initialValue={ person.email }/>,
+                <TextInput labelMsg="forms.person.phone" key="phone" name="phone"
+                    initialValue={ person.phone }/>,
+                <TextInput labelMsg="forms.person.altPhone" key="alt_phone" name="alt_phone"
+                    initialValue={ person.alt_phone }/>,
 
-                <TextInput labelMsg="forms.person.coAddress" name="co_address"
-                    initialValue={ person.co_address }/>
-                <TextInput labelMsg="forms.person.streetAddress" name="street_address"
-                    initialValue={ person.street_address }/>
-                <TextInput labelMsg="forms.person.zip" name="zip_code"
-                    initialValue={ person.zip_code }/>
-                <TextInput labelMsg="forms.person.city" name="city"
-                    initialValue={ person.city }/>
-                <TextInput labelMsg="forms.person.country" name="country"
-                    initialValue={ person.country }/>
+                <TextInput labelMsg="forms.person.coAddress" key="co_address" name="co_address"
+                    initialValue={ person.co_address }/>,
+                <TextInput labelMsg="forms.person.streetAddress" key="street_address" name="street_address"
+                    initialValue={ person.street_address }/>,
+                <TextInput labelMsg="forms.person.zip" key="zip_code" name="zip_code"
+                    initialValue={ person.zip_code }/>,
+                <TextInput labelMsg="forms.person.city" key="city" name="city"
+                    initialValue={ person.city }/>,
+                <TextInput labelMsg="forms.person.country" key="country" name="country"
+                    initialValue={ person.country }/>,
+            ];
 
-                <TextInput labelMsg="forms.person.externalId" name="ext_id"
-                    initialValue={ person.ext_id }/>
-            </Form>
-        );
+            const allFields = nativeFields.concat(fieldTypes.items
+                .filter(item => ['text', 'date', 'url'].includes(item.data.type))
+                .map(item => {
+                    const props = {
+                        initialValue: person[item.data.slug],
+                        key: item.data.slug,
+                        label: item.data.title,
+                        name: item.data.slug,
+                    };
+
+                    if (item.data.type == 'text') {
+                        return (
+                            <TextInput { ...props }/>
+                        );
+                    }
+                    else if (item.data.type == 'date') {
+                        return (
+                            <DateInput { ...props }/>
+                        );
+                    }
+                    else if (item.data.type == 'url') {
+                        return (
+                            <TextInput { ...props }/>
+                        );
+                    }
+                }));
+
+            return (
+                <Form ref="form" {...this.props }>
+                    { allFields }
+                </Form>
+            );
+        }
     }
 
     getValues() {
