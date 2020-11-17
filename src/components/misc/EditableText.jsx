ccaaa4323f5cbab9ebeb11b4c9f6cc76ffc9d73a
kristofferlarberg <mail@kristofferlarberg.se>
2020-11-17 17:52:19 +0100

    limitation for adding newlines

--- src/components/misc/EditableText.jsx
+++ src/components/misc/EditableText.jsx
@@ -59,6 +59,15 @@ export default class EditableText extends React.Component {
                     });
                 };
             },
+            onKeyDown: () => {
+                if (this.props.onKeyDown) {
+                    document.addEventListener('keydown', ev => {
+                        if (ev.key === 'Enter') {
+                            ev.preventDefault()
+                        }
+                    });
+                }
+            },
             dangerouslySetInnerHTML: {
                 __html: this.state.editing? this.props.content : (this.props.content || this.props.placeholder),
             },
