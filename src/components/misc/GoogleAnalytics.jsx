08892ea38e47865b8b9b7a9cf350bf4acb7e9065
Richard Olsson <r@richardolsson.se>
2020-12-11 09:58:03 +0100

    Externalize Google Analytics ID

--- src/components/misc/GoogleAnalytics.jsx
+++ src/components/misc/GoogleAnalytics.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 
 export default (props) => {
+    const measurementId = process.env.GA_MEASUREMENT_ID;
+
+    if (!measurementId) {
+        return null;
+    }
+
     const js = `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'G-4VXTJPS36L');
+        gtag('config', '${measurementId}');
     `;
 
     return (
