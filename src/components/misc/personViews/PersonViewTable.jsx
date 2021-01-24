76c6247cfb5dd9db3636e41832dfa372547d7005
Fanny <fannysarasoderlund@gmail.com>
2021-01-24 08:22:07 +0100

    indentation correction

--- src/components/misc/personViews/PersonViewTable.jsx
+++ src/components/misc/personViews/PersonViewTable.jsx
@@ -127,36 +127,36 @@ export default class PersonViewTable extends React.Component {
                     }
 
                     // Sort, if a column is selected for sorting
-            if (this.state.sortIndex !== null) {
-                visibleRows = visibleRows.concat().sort((row0, row1) => {
-                    const val0 = row0.data.content[this.state.sortIndex] || '';
-                    const val1 = row1.data.content[this.state.sortIndex] || '';
+                    if (this.state.sortIndex !== null) {
+                        visibleRows = visibleRows.concat().sort((row0, row1) => {
+                            const val0 = row0.data.content[this.state.sortIndex] || '';
+                            const val1 = row1.data.content[this.state.sortIndex] || '';
 
-                    let x = 0;
+                            let x = 0;
 
-                    if (Array.isArray(val0) && typeof(val0[0]) != 'undefined' && typeof(val1[0]) != 'undefined') {
-                        x = val0[0].text.toLowerCase().toString().localeCompare(val1[0].text.toLowerCase().toString());
-                    }
-                    else {
-                        x = val0.toString().localeCompare(val1.toString());
-                    }
+                            if (Array.isArray(val0) && typeof(val0[0]) != 'undefined' && typeof(val1[0]) != 'undefined') {
+                                x = val0[0].text.toLowerCase().toString().localeCompare(val1[0].text.toLowerCase().toString());
+                            }
+                            else {
+                                x = val0.toString().localeCompare(val1.toString());
+                            }
 
-                    if(typeof(val0[0]) == 'undefined' && typeof(val1[0]) != 'undefined') {
-                        return 1;
-                    }
-                    if(typeof(val0[0]) != 'undefined' && typeof(val1[0]) == 'undefined') {
-                        return -1;
-                    }
-                    if(typeof(val0[0]) == 'undefined' && typeof(val1[0]) == 'undefined') {
-                        return 0;
-                    }
+                            if(typeof(val0[0]) == 'undefined' && typeof(val1[0]) != 'undefined') {
+                                return 1;
+                            }
+                            if(typeof(val0[0]) != 'undefined' && typeof(val1[0]) == 'undefined') {
+                                return -1;
+                            }
+                            if(typeof(val0[0]) == 'undefined' && typeof(val1[0]) == 'undefined') {
+                                return 0;
+                            }
 
-                    if (this.state.sortInverted) {
-                        x *= -1;
-                    }
-                        return x;
-                });
-              }
+                            if (this.state.sortInverted) {
+                                x *= -1;
+                            }
+                                return x;
+                        });
+                      }
 
                     // Store match count for label
                     numMatches = visibleRows.length;
