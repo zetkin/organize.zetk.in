13efe080e7f782b699a698401fe37dd84bc0e866
Niklas Vanhainen <niklas.vanhainen@gmail.com>
2021-12-09 02:17:07 +0100

    User filter

--- src/components/filters/UserFilter.jsx
+++ src/components/filters/UserFilter.jsx
@@ -29,6 +29,11 @@ export default class UserFilter extends FilterBase {
         };
 
         return [
+            <FilterOrganizationSelect
+                config={ config } 
+                openPane={ this.props.openPane }
+                onChangeOrganizations={ this.onChangeOrganizations.bind(this) }
+                />,
             <SelectInput key="isUser" name="is_user"
                 labelMsg="filters.user.isUser.label"
                 options={ options } value={ value }
@@ -41,6 +46,8 @@ export default class UserFilter extends FilterBase {
     getConfig() {
         return {
             is_user: !!this.state.is_user,
+            organizationOption: this.state.organizationOption,
+            specificOrganizations: this.state.specificOrganizations,
         };
     }
 
