adf896d03ccb4bb51e4c992e4bc4b0f87a075d8a
Niklas Vanhainen <niklas.vanhainen@gmail.com>
2022-11-16 16:55:12 +0100

    Change survey options column to display options from most recent survey with responses.

--- src/components/misc/personViews/cells/SurveyOptionsCell.jsx
+++ src/components/misc/personViews/cells/SurveyOptionsCell.jsx
@@ -2,25 +2,38 @@ import React from 'react';
 import truncText from '../../../../utils/truncText';
 
 export default function SurveyOptionsCell(props) {
-    let options = [];
+    let rendered_options = [];
+    let hidden_options = [];
+    let latest = null;
     if (props.content && props.content.length) {
-        options = props.content.reduce((acc, option) => {
-            option.selected.forEach(s => {
-                if (!acc.find(x => s.id == x.id)) {
-                    acc.push(s);
-                }
-            });
-            return acc;
-        }, options);
+        latest = props.content.reduce((latest, submission) => 
+            !latest || 
+                submission.submission_id >= latest.submission_id && 
+                submission.selected.length ? submission : latest);
+        const options = latest.selected.sort((a,b) => a.id > b.id);
+        rendered_options = options.slice(0,2);
+        hidden_options = options.slice(2);
     }
     return (
         <td className={`SurveyOptionsCell`}>
-        { options.map(o => (
-            <li className="SurveyOption-text"
-                key={ o.id }>
-                { o.text }
-            </li>
-        )) }
+            <div className="SurveyOption-container"
+                onClick={ () => latest ? props.openPane('surveysubmission', latest.submission_id) : null }>
+                { rendered_options.map(o => (
+                    <div className="SurveyOption-option"
+                        key={ o.id }
+                        title={ o.text }>
+                        { o.text }
+                    </div>
+                )) }
+                { hidden_options.length ? (
+                    <div className="SurveyOption-more" 
+                        title={
+                            hidden_options.reduce((string, option) => string + option.text + '\n', '')
+                        }>
+                        +{ hidden_options.length }
+                    </div>
+                ) : null }
+            </div>
         </td>
     );
 }
